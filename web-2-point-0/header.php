528219939967c31cd329942b95c1f55ffa481e9f
adrajagopal <adrajagopal@gmail.com>
2023-06-24 16:12:37 -0500

    Flexbox the site header

--- web-2-point-0/header.php
+++ web-2-point-0/header.php
@@ -9,16 +9,14 @@
 			<span class="website-title">Ad Rajagopal</span>	
 		</div>
 
-		<div class="site-header">
-			<nav>
-				<ul class="site-header-items content-body">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="/projects">My work</a></li>
-					<li><a href="/get-in-touch">Get in touch</a></li>
-					<li><a href="https://adrajagopal.substack.com/">Substack ✈️</a></li>
-				</ul>
-			</nav>
-		</div>
+		<nav class="site-nav">
+			<ul class="site-header-items content-body">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="/projects">My work</a></li>
+				<li><a href="/get-in-touch">Get in touch</a></li>
+				<li><a href="https://adrajagopal.substack.com/">Substack ✈️</a></li>
+			</ul>
+		</nav>
 
 	</div>
 
