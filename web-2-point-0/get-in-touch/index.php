33af52931da60b3639e4341f90303fd24ab4346f
adrajagopal <adrajagopal@gmail.com>
2023-06-25 09:00:03 -0500

    Add copy to get-in-touch page

--- web-2-point-0/get-in-touch/index.php
+++ web-2-point-0/get-in-touch/index.php
@@ -3,8 +3,11 @@
 	include('../header.php');
 ?>
 
-<h1 class="hero-text">Hi there!</h1>
+<h1 class="hero-text">Want to connect?</h1>
 
+<p>If you'd like to talk about my work, my plans for a job, or anything else - get in touch.</p>
+
+<p>Send me an <a href="mailto:adrajagopal@gmail.com">email</a>, DM me on <a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/">LinkedIn</a>, or find me at one of my other links.</p>
 
 
 <?php
