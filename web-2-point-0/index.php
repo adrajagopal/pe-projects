db51204fffb2888c82578915a17ee935416e34e7
adrajagopal <adrajagopal@gmail.com>
2023-06-25 18:03:01 -0500

    Add content to index page

--- web-2-point-0/index.php
+++ web-2-point-0/index.php
@@ -3,9 +3,21 @@
 	include('header.php');
 ?>
 
-<h1 class="hero-text">Hi there!</h1>
+<h1 class="hero-title">Hi there!</h1>
 
+<h2 class="hero-content">I'm Ad. A product manager turned web developer.</h2>
 
+<h3 class="content-header">About me</h3>
+
+<p class="content-body">
+	I love making things - websites, spreadsheets, woodworks, dumb photoshop art, food stuffed inside other food. I loved being a product manager, but at the end of the day my hands weren't dirty enough. I crave the weeds. My greatest strengths are my attention to detail, organization, and proactivity. I'm also great at the development
+</p>
+
+<p class="content-body">
+	I'm a generalist extraordinaire, but most recently spent about 2.5 years as a product manager. I've always thought about going deep and specializing in something - something technical - and it took a full 5 years to learn about frontend and web development. It took another 2 years after that to burn out and work up the courage to give it a shot. You can read more about my journey <a href="https://adrajagopal.substack.com/p/the-peril-of-shiny-things" target="_blank">here</a>.
+</p>
+
+<p class="content-body">So this is me giving it a shot.</p>
 
 <?php
 	include('footer.php');
