6ccb712623c4fb8089d2ab02940030b5f9ed0446
adrajagopal <adrajagopal@gmail.com>
2023-06-25 09:00:32 -0500

    Add copy to get-in-touch

--- web-2-point-0/get-in-touch/index.php
+++ web-2-point-0/get-in-touch/index.php
@@ -5,7 +5,7 @@
 
 <h1 class="hero-text">Want to connect?</h1>
 
-<p>If you'd like to talk about my work, my plans for a job, or anything else - get in touch.</p>
+<p>If you'd like to talk about my work, my plans for a job, or anything else - get in touch. I'm open to any and all work opportunities - part-time through December 2023, and full-time starting January or February 2024.</p>
 
 <p>Send me an <a href="mailto:adrajagopal@gmail.com">email</a>, DM me on <a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/">LinkedIn</a>, or find me at one of my other links.</p>
 
