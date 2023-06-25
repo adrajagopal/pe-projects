26f68d6cccdea513ee2ccb50a17303353cf0c672
adrajagopal <adrajagopal@gmail.com>
2023-06-25 17:23:46 -0500

    Include config.php and base_url var

--- web-2-point-0/head.php
+++ web-2-point-0/head.php
@@ -1,8 +1,11 @@
+<?php include("config.php");?>
+
 <!doctype html>
 
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<base href="<?=BASE_URL?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 
