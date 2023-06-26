707ab06e42cc2e2b4030377b3dbb5b8cd4821c8d
adrajagopal <adrajagopal@gmail.com>
2023-06-26 16:33:59 -0500

    Apply font classes

--- web-2-point-0/get-in-touch/index.php
+++ web-2-point-0/get-in-touch/index.php
@@ -3,11 +3,11 @@
 	include('../header.php');
 ?>
 
-<h1 class="hero-text">Want to connect?</h1>
+<h1 class="hero-title">Want to connect?</h1>
 
-<p>If you'd like to talk about my work, my plans for a job, or anything else - get in touch. I'm open to any and all work opportunities - part-time through December 2023, and full-time starting January or February 2024.</p>
+<p class="content-body">If you'd like to talk about my work, my plans for a job, or anything else - get in touch. I'm open to any and all work opportunities - part-time through December 2023, and full-time starting January or February 2024.</p>
 
-<p>Send me an <a href="mailto:adrajagopal@gmail.com">email</a>, DM me on <a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/" target="_blank">LinkedIn</a>, or find me at one of my other links.</p>
+<p class="content-body">Send me an <a href="mailto:adrajagopal@gmail.com">email</a>, DM me on <a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/" target="_blank">LinkedIn</a>, or find me at one of my other links.</p>
 
 
 <?php
