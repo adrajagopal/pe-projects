b069b037cf45e446cd58709cbaa87ff2236ea9b2
adrajagopal <adrajagopal@gmail.com>
2023-06-28 13:27:11 -0400

    Download font-awesome icons, create php file

---
+++ web-2-point-0/socials.php
@@ -0,0 +1,13 @@
+<nav class="socials">
+	<a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/" target="_blank">
+		<svg class="icon-"><use xlink:href="#icon-linkedin-square"></use></svg>
+	</a>
+	
+	<a href="mailto:adrajagopal@gmail.com" target="_blank">
+		<svg class="icon-"><use xlink:href="#icon-envelope"></use></svg>
+	</a>
+	
+	<a href="https://github.com/adrajagopal" target="_blank">
+		<svg class="icon-"><use xlink:href="#icon-github"></use></svg>
+	</a>
+</nav>
