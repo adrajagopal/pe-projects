6362c79f6ecd6a132e9d66cf62aeed8a1b6810c5
adrajagopal <adrajagopal@gmail.com>
2023-06-28 13:27:25 -0400

    Update footer social links

--- web-2-point-0/footer.php
+++ web-2-point-0/footer.php
@@ -4,9 +4,7 @@
 	<footer class="site-footer">
 
 		<div class="footer-list inner-column">
-			<a href="mailto:adrajagopal@gmail.com" target="_blank">Email</a>
-			<a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/" target="_blank">LinkedIn</a>
-			<a href="https://github.com/adrajagopal" target="_blank">GitHub</a>
+			<?php include('socials.php');?>
 		</div>
 
 	</footer>
