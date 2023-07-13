eb292cc46bc01cd6960040adcdfffca237fd64eb
adrajagopal <adrajagopal@gmail.com>
2023-07-13 10:10:33 -0500

    Fix test subpage and homepage navigation

--- test/subpage/index.php
+++ test/subpage/index.php
@@ -5,16 +5,17 @@
 	<head>
 		<base href="<?=BASE_URL?>">
 		
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="../style.css">
 	</head>
 
 	<body>
 
 		<h1>this is a title</h1>
 
-		<a href="">Home</a>
-		<a href="subpage">Subpage</a>
+		<a href="../">Home</a>
+		<a href="../subpage">Subpage</a>
 
 	</body>
 
 </html>
+
