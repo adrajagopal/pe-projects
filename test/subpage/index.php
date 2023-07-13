671660b7b179c8d22bd6a335b5f1ccb5623aea89
adrajagopal <adrajagopal@gmail.com>
2023-07-13 09:33:42 -0500

    Add test code to subpage

--- test/subpage/index.php
+++ test/subpage/index.php
@@ -1,1 +1,17 @@
-index.php
+<html>
+	
+
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+
+		<h1>this is a title</h1>
+
+		<a href="">Home</a>
+		<a href="subpage">Subpage</a>
+
+	</body>
+
+</html>
