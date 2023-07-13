62f586cac358293486e90a6f3ee7ea978e5e0e38
adrajagopal <adrajagopal@gmail.com>
2023-07-13 10:15:10 -0500

    Break header into php partial

---
+++ test/header.php
@@ -0,0 +1,12 @@
+<?php DEFINE("BASE_URL", "https://peprojects.dev/alpha-7/ad/test/");?>
+
+<html>
+	
+
+	<head>
+		<base href="<?=BASE_URL?>">
+		
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
--- test/index.php
+++ test/index.php
@@ -1,15 +1,4 @@
-<?php DEFINE("BASE_URL", "https://peprojects.dev/alpha-7/ad/test/");?>
-
-<html>
-	
-
-	<head>
-		<base href="<?=BASE_URL?>">
-		
-		<link rel="stylesheet" href="style.css">
-	</head>
-
-	<body>
+<?php include("header.php"); ?>
 
 		<h1>this is a title</h1>
 
@@ -20,8 +9,5 @@
 
 
 	</body>
-
-
-
 
 </html>
