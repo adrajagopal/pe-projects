f6c1dca7ee45f1cfe9e28b9e314fd2702ece1c2a
adrajagopal <adrajagopal@gmail.com>
2023-07-13 09:31:24 -0500

    Create test homepage w/ new base_url

---
+++ test/index.php
@@ -0,0 +1,27 @@
+<?php DEFINE("BASE_URL", "https://peprojects.dev/alpha-7/ad/test/");?>
+
+<html>
+	
+
+	<head>
+		<base href="<?=BASE_URL?>">
+		
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+
+		<h1>this is a title</h1>
+
+		<a href="">Home</a>
+		<a href="subpage">Subpage</a>
+
+
+
+
+	</body>
+
+
+
+
+</html>
