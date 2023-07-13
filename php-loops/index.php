2396668a93566b59c0164af68bc75a14c383c610
adrajagopal <adrajagopal@gmail.com>
2023-07-13 14:40:40 -0500

    Add dog bios

--- php-loops/index.php
+++ php-loops/index.php
@@ -14,7 +14,8 @@
 		"age" => "12",
 		"coatType" => "wire",
 		"isAsshole" => "yes",
-		"portrait" => "php-loops/images/buddha.jpg"
+		"portrait" => "php-loops/images/buddha.jpg",
+		"bio" => "Buddha might technically be a chiweenie, but really he's a silver fox. In his golden years, he likes to sit in the sun and watch the wind blow. He loves people, coexists peacefully with most dogs, and loves cats in an often unreciprocated manner. He has a healthy love for chicken and fish, and will love you forever if you have long nails and a heating pad."
 	];
 
 	$minnie = [
@@ -27,7 +28,8 @@
 		"age" => "2",
 		"coatType" => "straight",
 		"isAsshole" => "no",
-		"portrait" => "php-loops/images/minnie.jpg"
+		"portrait" => "php-loops/images/minnie.jpg",
+		"bio" => "Minnie is a terror and a joy. She can be skittish around new people, places, sounds, animals, and environments. But once she's comfortable in her surroundings, she is a joy to have around. She loves long walks, treeing squirrels, wrestling and playing rough with you and other dogs, and is obsessed with peanut butter. Minnie's cutest trait is that she sighs and grumbles in her sleep - very loudly."
 	];
 
 	$chubs = [
@@ -40,7 +42,8 @@
 		"age" => "10",
 		"coatType" => "straight",
 		"isAsshole" => "no",
-		"portrait" => "php-loops/images/chubs.jpg"
+		"portrait" => "php-loops/images/chubs.jpg",
+		"bio" => "Chubs was a pure lover. He loved all people and was a jealous attention hog. He loved playing with bouncy balls and howling at postal workers and delivery trucks. He could recognize the sound of the UPS truck engine and squealing brakes from a half-block away, so we always knew when a package was coming - for anyone in our neighborhood. He had a tough last year of his life, but was still happy until the last day."
 	];
 
 	$myDogs = [$buddha, $chubs, $minnie];
@@ -58,13 +61,14 @@
 		foreach ($myDogs as$dog) {
 			echo
 				"<div class='dog-box'>" .
-				"<h3 class='large-voice content-header'>" . $dog["name"] . "</h3>"
+				"<h3 class='xl-voice content-header'>" . $dog["name"] . "</h3>"
 				. "<picture class='dog-portrait'><img src=" . $dog["portrait"] . "></picture>"
-				. "<ul class='medium-voice content-subheader'>"
+				. "<ul class='medium-voice content-body'>"
 				. "<li>" . "Age: " . $dog["age"] . "</li>"
 				. "<li>" . "Sex: " . $dog["sex"] . "</li>"
 				. "<li>" . "Breed: " . $dog["breed"] . "</li>"
 				. "</ul>"
+				. "<p class='medium-voice content-body'>" . $dog["bio"] . "</p>"
 				. "</div>"
 				;
 		}
