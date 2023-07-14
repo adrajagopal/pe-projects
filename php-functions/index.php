2c124b1d679b9a41f167b73cc94b167d186de2db
adrajagopal <adrajagopal@gmail.com>
2023-07-14 08:18:11 -0500

    Create functions directory for new lesson

---
+++ php-functions/index.php
@@ -0,0 +1,45 @@
+<?php
+	include('../head.php');
+	include('../header.php');
+?>
+
+
+
+<h1 class="xxl-voice hero-title">hi there friend</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+	include('../footer.php');
+?>
