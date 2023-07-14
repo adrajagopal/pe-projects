910e1207cf30d20cef8c876d34e97ea974074a4c
adrajagopal <adrajagopal@gmail.com>
2023-07-14 11:07:26 -0500

    Move code from loops to functions page

--- php-functions/index.php
+++ php-functions/index.php
@@ -9,6 +9,37 @@
 
 
 
+</ul>
+
+
+<?php
+	
+	$pmtOptions = [
+		"high" => 0.25,
+		"med" => 0.15,
+		"low" => 0.05,
+	];
+
+	function amount($bal, $value) {
+			echo $bal * $value;
+		} ?>
+
+<ul class="medium-voice content-body">
+	
+	<?php
+		foreach ($pmtOptions as $option => $value) { ?>
+
+
+		<li style="list-style-type: disc; margin-left: 20px;">
+			If you go with a <?=$option?> interest rate (<?=$value*100?>%), your payment will be : $<?=amount(1250, $value)?>
+		</li>
+			
+	<?php	} ?>
+	
+
+
+</ul>
+
 
 
 
