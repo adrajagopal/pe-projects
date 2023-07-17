77631892d6ea2bfbfcb2463a5add1f49c5b8c8a0
adrajagopal <adrajagopal@gmail.com>
2023-07-17 09:51:06 -0500

    Create site footer

--- footer.php
+++ footer.php
@@ -1,1 +1,13 @@
-header.php
+	</main>
+
+<!-- Site footer-------------------------------------- -->
+	<footer class="site-footer">
+
+		<div class="footer-list inner-column">
+			<?php include('socials.php');?>
+		</div>
+
+	</footer>
+
+</body>
+</html>
