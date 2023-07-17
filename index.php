c37a529adb679c1b0172a6518a859c6b4954418c
adrajagopal <adrajagopal@gmail.com>
2023-07-17 11:04:22 -0500

    New About Me on homepage

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php include("header.php");?>
 
 
-<h1>Hi</h1>
+<h1 class="hero-title xl-voice">I'm Ad. A product manager turned web developer.</h1>
 
+<h3 class="content-header large-voice">My work</h3>
+
+<h3 class="content-header large-voice">About me</h3>
+
+
+<p>I'm a visually-oriented developer who loves making great designs come alive. I have a background in analytics, visual design and product management, and I'm looking for a development role where I can contribute throughout the build cycle -- including <em>before</em> any code gets written.</p>
+
+<p>I love working with designers, product managers (I used to be one, so I'm not afraid of them 😉), and engineers; I thrive on early and frequent feedback. My greatest strengths are my attention to detail, organization, and proactivity.</p>
+
+<p>If you'd like to work together, talk about <a href="https://github.com/adrajagopal" target="_blank">my work</a>, or just chat about design & code, get in touch with me via <a href="mailto:adrajagopal@gmail.com" target="_blank">email</a> or <a href="https://www.linkedin.com/in/aditya-rajagopal-a68b8281/" target="_blank">LinkedIn</a>.</p>
+
+<iframe src="https://adrajagopal.substack.com/embed" width="320" height="180" style="border:1px solid var(--ink-dark); background:var(--splash-primary);" frameborder="0" scrolling="no"></iframe>
 
 <?php include("footer.php")?>
