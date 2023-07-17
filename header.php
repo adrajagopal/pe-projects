c7655f01886fb291742fd7f2f0b565fef349a9c3
adrajagopal <adrajagopal@gmail.com>
2023-07-17 09:08:47 -0500

    Create new head+header file

--- header.php
+++ header.php
@@ -1,1 +1,68 @@
-header.php
+<?php include("config.php");?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<base href="<?=BASE_URL?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+
+<!-- delete this comment once you've replaced the template content -->
+	<title>Ad Rajagopal</title>
+
+<!-- delete this comment once you've replaced the template content -->
+	<meta name="description" content="This is some page. You hit it.">
+	
+<!-- delete this comment once you've replaced the template content -->
+	<meta property="og:title" content="Ad's Portfolio'">
+
+<!-- delete this comment once you've replaced the template content -->
+	<!-- <meta
+	  property="og:description"
+	  content="[[INSERT CONTENT]]"> -->
+
+<!-- delete this comment once you've replaced the template content -->
+	<meta
+	  property="og:image"
+	  content="[[INSERT CONTENT]].png">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+
+<!-- 	fonts used:
+	Baskerville 400, 400 + ital
+	Work Sans 100, 100 + ital, 400, 400 + ital, 600, 700 -->
+	<link href="https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Work+Sans:ital,wght@0,100;0,400;0,600;0,700;1,100;1,400&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	
+<?php
+	include('icons.php');
+?>
+
+<!-- Site header-------------------------------------- -->
+	<header class="site-header">
+
+	<div class="header-items inner-column">
+
+		<div class="brand-slot">
+			<span class="website-title">Ad Rajagopal</span>	
+		</div>
+
+		<nav class="site-nav">
+			<ul class="site-nav-items">
+				<li><a href="">Home</a></li>
+				<li><a href="goals">My goals</a></li>
+				<li><a href="get-in-touch">Get in touch</a></li>
+				<li><a href="https://adrajagopal.substack.com/" target="_blank">Substack ✈️</a></li>
+			</ul>
+		</nav>
+
+	</div>
+
+	</header>
+
+	<main class="page-wrapper inner-column">
