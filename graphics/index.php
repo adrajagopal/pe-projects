b76b5b4e0724919157baad4e2715219f4cf30961
adrajagopal <adrajagopal@gmail.com>
2023-07-17 12:11:00 -0500

    Create Affinity work page on website

---
+++ graphics/index.php
@@ -0,0 +1,19 @@
+<?php
+	include("../header.php");
+?>
+
+<h1 class="hero-title xxl-voice">Graphics</h1>
+
+<h2 class="content-header xl-voice">Working with graphics</h2>
+
+<p class="content-body medium-voice">Working on graphics is a great way to learn about preparing images for display on websites and applications. Among the things I can do with graphics software & code: make files smaller without losing quality, pick the right image type for the job (PNG, JPG, SVG), make GIFs, export SVGs and clean up the code, and create quick graphics mockups.</p>
+
+<p class="content-body medium-voice">Don't call me a graphics expert, but I can scrap some nice-looking things together.</p>
+
+<h3 class="content-header xl-voice">My creations</h3>
+
+
+
+<?php
+	include('../footer.php');
+?>
