35843c1874d2e7af4deecd7267acfaee16383edd
adrajagopal <adrajagopal@gmail.com>
2023-07-17 14:45:53 -0500

    Update fu-gallery item descriptions

--- graphics/index.php
+++ graphics/index.php
@@ -18,11 +18,11 @@
 
 
 <?php
-	$avocado = ["name" => "Avocado", "description" => "The happiest avocado.", "portrait" => "graphics/images/bf-avocado.jpg"];
-	$banana = ["name" => "Banana", "description" => "Watch out for the indifferent banana.", "portrait" => "graphics/images/bf-banana.jpg"];
-	$eggplant = ["name" => "Eggplant", "description" => "Eggplants just seem sad naturally, don't they?.", "portrait" => "graphics/images/bf-eggplant.jpg"];
-	$milk = ["name" => "Milk", "description" => "Sure, milk.", "portrait" => "graphics/images/bf-milk.jpg"];
-	$watermelon = ["name" => "Watermelon", "description" => "Ominous - for sure..", "portrait" => "graphics/images/bf-watermelon.jpg"];
+	$avocado = ["name" => "Avocado", "description" => "Pure joy", "portrait" => "graphics/images/bf-avocado.jpg"];
+	$banana = ["name" => "Banana", "description" => "Aggressive indifference", "portrait" => "graphics/images/bf-banana.jpg"];
+	$eggplant = ["name" => "Eggplant", "description" => "Innate sadness", "portrait" => "graphics/images/bf-eggplant.jpg"];
+	$milk = ["name" => "Milk", "description" => "Sure, milk", "portrait" => "graphics/images/bf-milk.jpg"];
+	$watermelon = ["name" => "Watermelon", "description" => "Devious melon", "portrait" => "graphics/images/bf-watermelon.jpg"];
 	$me = ["name" => "Me", "description" => "This one is just me.", "portrait" => "graphics/images/bf-me.jpg"];
 
 	$boomerangFuCharacters = [$avocado, $banana, $eggplant, $milk, $watermelon, $me];
