006ed78043aa90cc481f8e6ba4c2821f63d37b8a
adrajagopal <adrajagopal@gmail.com>
2023-07-17 10:10:29 -0500

    Change CSS reference back to style.css

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 	  property="og:image"
 	  content="[[INSERT CONTENT]].png">
 	
-	<link type="text/css" rel="stylesheet" href="css/bonk.css">
+	<link rel="stylesheet" href="css/style.css">
 
 <!-- 	fonts used:
 	Baskerville 400, 400 + ital
