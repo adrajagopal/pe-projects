3e95282952e0c23c67f2d3a05f253cec68fc21d2
adrajagopal <adrajagopal@gmail.com>
2023-07-19 20:37:17 -0500

    Create test array for jobs on resume

---
+++ resume-job-list.php
@@ -0,0 +1,16 @@
+<?php
+	$jobs = [
+		[
+			"role" => "Product Manager",
+			"organization" => "Oshi Health",
+			"tenure" => [
+				"startDate" => date_create("01-05-2021"),
+				"endDate" => date_create("27-01-2023")
+			],
+			"actions" => [
+				"Joined as the second member of the product team, directly responsible for mobile app, web app, and web platform products. Led product operations from seed stage through Series A and in preparation for Series B fundraising",
+				"Played many roles, working on everything from typical product tasks to user research, data analysis, project management, QA/feature testing, copy & design reviews",
+				"Improved product quality - in other words, my features shipped more predictably and with fewer bugs - through clear product documentation, increased transparency with stakeholders, better collaboration with design and engineering, and more getting in the weeds to test",
+				"Drove key metrics with major releases: reduced appointment no-shows/late cancelations, reduced overhead on routine tasks for clinical support staff, and simplified time to complete intake paperwork",
+			]
+		],
