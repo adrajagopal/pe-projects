5fd430515e89ce7438e47ae128e427d81c3be8ad
adrajagopal <adrajagopal@gmail.com>
2023-07-20 09:54:38 -0500

    create base form structure

--- php-character-count/index.php
+++ php-character-count/index.php
@@ -3,7 +3,19 @@
 
 <h1 class="hero-title xl-voice">Character counts.</h1>
 
-<h2 class="content-header large-voice">Count your characters.</h2>
+<form action="">
+
+	<h2 class="content-header large-voice">Count your characters.</h2>	
+
+	<div class="field">
+		<label class="medium-voice" for="">Your string</label>
+		<input type="text" name="string" placeholder="Type or paste in..." value="<?=$characters?>">
+	</div>
+	
+	<button type="submit">Count</button>
+
+
+</form>
 
 
 <?php include("../footer.php")?>
