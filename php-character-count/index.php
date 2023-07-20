173c280195f9e18a2c32447baf1178abd84667be
adrajagopal <adrajagopal@gmail.com>
2023-07-20 09:38:00 -0500

    Create directory and index file

---
+++ php-character-count/index.php
@@ -0,0 +1,9 @@
+<?php include("../header.php");?>
+
+
+<h1 class="hero-title xl-voice">Character counts.</h1>
+
+<h2 class="content-header large-voice">Count your characters.</h2>
+
+
+<?php include("../footer.php")?>
