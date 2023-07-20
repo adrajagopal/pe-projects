b09d2e4cfe80cb2f2e971b9f138a9aa93f0d0baa
adrajagopal <adrajagopal@gmail.com>
2023-07-20 17:48:11 -0500

    Create form skeleton

--- paint-calculator/index.php
+++ paint-calculator/index.php
@@ -11,5 +11,30 @@
 
 <p class="form-context">We built this calculator to help you paint rooms that are perfectly rectangular with no nooks, no crannies, and no protuberant closets.</p>
 
+<form action="">
+	<div class="field">
+		<label for="">Room length</label>
+		<input type="text">
+		<input type="text">
+	</div>
+	<div class="field">
+		<label for="">Room width</label>
+		<input type="text">
+		<input type="text">
+	</div>
+	<button>Calculate</button>
+</form>
+
+<!-- 
+	* Ask user for length in feet and inches
+	* Ask user for width in feet and inches
+	* Convert inches to a fraction and add to feet
+	* Multiply length times width
+	* Divide by 350
+	* Round up to the nearest integer ←this is gallons needed
+	* Round square footage to the second float ← this is square footage
+	* Tell the user how many square feet they have and how many gallons of paint they need
+-->
+
 
 <?php include("../footer.php");?>
