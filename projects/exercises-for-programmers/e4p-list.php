4be20fdd876789ceeb592b77068c7a82ecb3acf2
adrajagopal <adrajagopal@gmail.com>
2023-07-21 07:57:46 -0500

    Create array for e4p projects

---
+++ projects/exercises-for-programmers/e4p-list.php
@@ -0,0 +1,28 @@
+<?php
+
+$e4pList = [
+	$e4pCharCount = [
+		"name" => "Character Counter",
+		"description" => "Count the number of characters in a string.",
+		"link" => "exercises-for-programmers/character-count"
+	],
+	$e4pPaintCalculator = [
+		"name" => "Paint Calculator",
+		"description" => "Calculates the number of gallons, rounded up, needed to paint a rectangular room.",
+		"link" => "exercises-for-programmers/paint-calculator"
+	]
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
