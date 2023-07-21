a7a25a9408565f8b133cfaa795c709772b0ae21c
adrajagopal <adrajagopal@gmail.com>
2023-07-21 07:48:05 -0500

    Create projects page

---
+++ projects/index.php
@@ -0,0 +1,13 @@
+<?php include("header.php");?>
+
+
+<h1 class="hero-title xl-voice">Code</h1>
+
+<h2 class="content-header large-voice">Here's a selection of projects.</h2>
+
+
+
+
+
+
+<?php include("footer.php")?>
