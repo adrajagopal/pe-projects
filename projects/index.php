3205971e49b084ac6837700994528aa85bd3c7f5
adrajagopal <adrajagopal@gmail.com>
2023-07-21 07:53:48 -0500

    Create base copy

--- projects/index.php
+++ projects/index.php
@@ -1,13 +1,22 @@
-<?php include("header.php");?>
+<?php include("../header.php");?>
 
 
-<h1 class="hero-title xl-voice">Code</h1>
+<h1 class="hero-title xxl-voice">Code</h1>
 
-<h2 class="content-header large-voice">Here's a selection of projects.</h2>
+<h2 class="content-header xl-voice">Here's a selection of projects.</h2>
+
+<h3 class="content-subheader large-voice">Exercises for Programmers</h3>
+
+<p class="content-body medium-voice">
+	<a href="https://pragprog.com/titles/bhwb/exercises-for-programmers/" target="_blank">Exercises for Programmers</a> is a book of more than 50 practice exercises with real applications. Here are a few I've done.
+</p>
+
+<ul class="content-body medium-voice">
+	
+</ul>
 
 
 
 
 
-
-<?php include("footer.php")?>
+<?php include("../footer.php")?>
