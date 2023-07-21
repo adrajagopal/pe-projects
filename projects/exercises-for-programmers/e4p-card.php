3ce794b1cf90bc0aefb446b8980df0a9bc498bd0
adrajagopal <adrajagopal@gmail.com>
2023-07-21 10:27:29 -0500

    Move e4p card html & logic to its own file

---
+++ projects/exercises-for-programmers/e4p-card.php
@@ -0,0 +1,8 @@
+<?php		
+	$name = $project["name"];
+	$description = $project["description"];
+	$link = $project["link"];
+?>
+	<li>
+		<p><a href="projects/exercises-for-programmers/<?=$link?>"><?=$name?></a>: <?=$description?></p>
+	</li>
