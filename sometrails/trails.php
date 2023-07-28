9d26a1c230a6845f217491561f2d4f6fde83050b
adrajagopal <adrajagopal@gmail.com>
2023-07-28 18:13:07 -0500

    Create gallery card

--- sometrails/trails.php
+++ sometrails/trails.php
@@ -8,4 +8,21 @@
 	<p>After that, create details page with querystring routing</p>
 </div>
 
+
+<section class="inner-column">
+	<h2 class="xl-voice">Find your next hike.</h2>
+
+	<div class="gallery-card">
+		<a href="">
+			<picture><img src="images/sometrails-icon.jpg" alt=""></picture>
+		</a>
+		<div class="gallery-card-text">
+			<p class="large-voice">Trail Name</p>
+			<p class="medium-voice">Here's a short description of the trail.</p>
+			<a href="" class="medium-voice">Get more info</a>
+		</div>
+	</div>
+
+</section>
+
 <?php include("footer.php");?>
