486b9ec39a2a3913243b8ee45cf001a102391a57
adrajagopal <adrajagopal@gmail.com>
2023-07-28 18:44:38 -0500

    Create gallery card content and styling

--- sometrails/trails.php
+++ sometrails/trails.php
@@ -12,15 +12,44 @@
 <section class="inner-column">
 	<h2 class="xl-voice">Find your next hike.</h2>
 
-	<div class="gallery-card">
-		<a href="">
-			<picture><img src="images/sometrails-icon.jpg" alt=""></picture>
-		</a>
-		<div class="gallery-card-text">
-			<p class="large-voice">Trail Name</p>
-			<p class="medium-voice">Here's a short description of the trail.</p>
-			<a href="" class="medium-voice">Get more info</a>
+	<div class="gallery inner-column">
+
+		<div class="gallery-card">
+			<a href="" class="clickable-pic">
+				<picture><img src="images/sometrails-icon.jpg" alt=""></picture>
+			</a>
+			<div class="gallery-card-text">
+				<p class="large-voice">Trail Name</p>
+				<p class="medium-voice">Here's a short description of the trail. Here's a short description of the trail. Here's a short description of the trail. Here's a short description of the trail.</p>
+				<a href="" class="medium-voice">Get more info</a>
+			</div>
 		</div>
+
+				<div class="gallery-card">
+			<a href="" class="clickable-pic">
+				<picture><img src="images/sometrails-icon.jpg" alt=""></picture>
+			</a>
+			<div class="gallery-card-text">
+				<p class="large-voice">Trail Name</p>
+				<p class="medium-voice">Here's a short description of the trail. Here's a short description of the trail.</p>
+				<a href="" class="medium-voice">Get more info</a>
+			</div>
+		</div>
+
+
+		<div class="gallery-card">
+			<a href="" class="clickable-pic">
+				<picture><img src="images/sometrails-icon.jpg" alt=""></picture>
+			</a>
+			<div class="gallery-card-text">
+				<p class="large-voice">Trail Name</p>
+				<p class="medium-voice">Here's a short description of the trail.</p>
+				<a href="" class="medium-voice">Get more info</a>
+			</div>
+		</div>
+
+
+
 	</div>
 
 </section>
