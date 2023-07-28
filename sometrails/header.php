bf530812fc437e91ad0e79f9fb0fc292392ae0a0
adrajagopal <adrajagopal@gmail.com>
2023-07-28 17:48:59 -0500

    test3

---
+++ sometrails/header.php
@@ -0,0 +1,35 @@
+<?php include("../config.php"); ?>
+
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+
+	<base href="<?=BASE_URL?>/sometrails/">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>SomeTrails</title>
+
+	<meta name="description" content="Explore Wisconsin's best trails.">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+	
+
+	<header>
+		<section class="inner-column">
+			<a href=""><picture><img src="images/sometrails-logo.png" alt="SomeTrails logo"></picture></a>
+			<nav>
+				<a href="trails.php">Trails A-Z</a>
+			</nav>		
+	</header>
+
+	<main class="page-wrapper">
---
+++ sometrails/index.php
@@ -0,0 +1,10 @@
+<?php include("header.php");?>
+
+<div class="inner-column">
+
+	<h1 class="xxl-voice">SomeTrails</h1>
+	<h2 class="xl-voice">Explore trails. Some of them.</h2>
+	<p>This is a paragraph to test whether my font is coming in.</p>	
+</div>
+
+<?php include("footer.php");?>
