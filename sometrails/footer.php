0e5910bce209c0893d73033245f6c5dfda3c1be1
adrajagopal <adrajagopal@gmail.com>
2023-07-28 15:40:01 -0500

    Write initial footer code

--- sometrails/footer.php
+++ sometrails/footer.php
@@ -1,6 +1,11 @@
 </main>
 
-<h1>Footer</h1>
+<footer>
+	<section class="inner-column">
+		<picture class="app-icon"><img src="images/sometrails-icon.png" alt="SomeTrails app logo"></picture>
+		<p>Get the app!</p>
+	</section>
+</footer>
 
 </body>
 
