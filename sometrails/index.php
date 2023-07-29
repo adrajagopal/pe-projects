e3ee006f555e09c411ef584e23167174c57f3a89
adrajagopal <adrajagopal@gmail.com>
2023-07-29 17:26:33 -0500

    Delete test text

--- sometrails/index.php
+++ sometrails/index.php
@@ -4,7 +4,7 @@
 
 	<h1 class="xxl-voice">SomeTrails</h1>
 	<h2 class="xl-voice">Explore trails. Some of them.</h2>
-	<p>This is a paragraph to test whether my font is coming in.</p>	
+	
 </div>
 
 <?php include("footer.php");?>
--- sometrails/trail-gallery.php
+++ sometrails/trail-gallery.php
@@ -5,10 +5,6 @@
 
 <div class="inner-column">
 	<h1 class="xxl-voice">Trails</h1>
-
-	<p>Next up for this page - set up a trail gallery</p>
-
-	<p>After that, create details page with querystring routing</p>
 </div>
 
 
