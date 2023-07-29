064f52019cdc1f316ad414c763ba9ccdf5c29cc9
adrajagopal <adrajagopal@gmail.com>
2023-07-29 17:09:34 -0500

    formatting

--- sometrails/trail-details-card.php
+++ sometrails/trail-details-card.php
@@ -1,12 +1,17 @@
 <picture><img src="<?=$trail['portrait']?>" alt=""></picture>
+
 <h1 class="xl-voice"><?=$trail['name']?></h1>
+
 <h2 class="large-voice"><?=$trail['city']?></h2>
+
 <ul class="medium-voice">
 	<li>Type: <?=$trail['type']?></li>
 	<li>Length: <?=$trail['length']?></li>
 	<li>Dog-friendly: <?=$trail['dog-friendly']?></li>
 </ul>
+
 <p class="medium-voice"><?=$trail['description']?></p>
+
 <div class="activity-box">
 	<ul class="activities medium-voice">
 		<?php foreach ($trail['activities'] as $trail_activity) { ?>
