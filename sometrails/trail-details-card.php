390319bb5a71cb05b242c71915c70a6a7e78653b
adrajagopal <adrajagopal@gmail.com>
2023-07-29 17:25:06 -0500

    Put div around trail-details-card for styling

--- sometrails/trail-details-card.php
+++ sometrails/trail-details-card.php
@@ -1,21 +1,29 @@
-<picture><img src="<?=$trail['portrait']?>" alt=""></picture>
 
-<h1 class="xl-voice"><?=$trail['name']?></h1>
+<div class="trail-details-card">
+	<picture><img src="<?=$trail['portrait']?>" alt=""></picture>
 
-<h2 class="large-voice"><?=$trail['city']?></h2>
+	<h1 class="xl-voice"><?=$trail['name']?></h1>
 
-<ul class="medium-voice">
-	<li>Type: <?=$trail['type']?></li>
-	<li>Length: <?=$trail['length']?></li>
-	<li>Dog-friendly: <?=$trail['dog-friendly']?></li>
-</ul>
+	<h2 class="large-voice"><?=$trail['city']?></h2>
 
-<p class="medium-voice"><?=$trail['description']?></p>
+	<div class="trail-details">
+		<ul class="medium-voice">
+			<li><strong>Type:</strong> <?=$trail['type']?></li>
+			<li><strong>Length:</strong> <?=$trail['length']?></li>
+			<li><strong>Dog-friendly:</strong> <?=$trail['dog-friendly']?></li>
+		</ul>
 
-<div class="activity-box">
-	<ul class="activities medium-voice">
-		<?php foreach ($trail['activities'] as $trail_activity) { ?>
-			<li><?=$trail_activity?></li>
-		<?php } ?>
-	</ul>
+		<p class="medium-voice"><?=$trail['description']?></p>
+
+		<p class="medium-voice"><strong>Activities:</strong></p>
+
+		<div class="activity-box medium-voice">
+			<ul class="activities medium-voice">
+				<?php foreach ($trail['activities'] as $trail_activity) { ?>
+					<li><?=$trail_activity?></li>
+				<?php } ?>
+			</ul>
+		</div>
+	</div>
+
 </div>
