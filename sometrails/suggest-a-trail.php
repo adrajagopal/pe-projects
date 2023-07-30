acfe42479e6c67578e3a0ee8ca7aa549509416ae
adrajagopal <adrajagopal@gmail.com>
2023-07-30 13:18:17 -0500

    Build in form validation logic & copy

--- sometrails/suggest-a-trail.php
+++ sometrails/suggest-a-trail.php
@@ -5,6 +5,33 @@
 	$submissionCity ="";
 	$submissionReason ="";
 	$submissionReference ="";
+	$nameError = false;
+	$cityError = false;
+	$reasonError = false;
+	$referenceError = false;
+
+	if(isset($_POST["submitted"])) {
+		$submissionFields = [
+			$submissionName = $_POST["submissionName"],
+			$submissionCity =$_POST["submissionCity"],
+			$submissionReason =$_POST["submissionReason"],
+			$submissionReference =$_POST["submissionReference"]
+		];
+
+		if (isset($submissionName) == false OR strlen($submissionName) === 0) {
+			$nameError = true;
+		}
+		if (isset($submissionCity) == false OR strlen($submissionCity) === 0) {
+			$cityError = true;
+		}
+		if (isset($submissionReason) == false OR strlen($submissionReason) === 0) {
+			$reasonError = true;
+		}
+		if (isset($submissionReference) == false OR strlen($submissionReference) === 0) {
+			$referenceError = true;
+		}
+	}
+
 ?>
 
 <section class="inner-column">
@@ -16,26 +43,34 @@
 		<fieldset class="field short-text">
 			<legend class="large-voice">What's the name of the trail?</legend>
 			<input type="text" name="submissionName">
-			<!-- <p class="warning">Please include the trail name</p> -->
+			<?php if (isset($nameError) & $nameError == true) {
+				echo "<p class='warning'>Please enter a city name.</p>";
+			} ?>
 		</fieldset>
 		
 		<fieldset class="field short-text">
 			<legend class="large-voice">What city is the trail in, or what city is nearest?</legend>
 			<input type="text" name="submissionCity">
-			<!-- <p class="warning">Please include the location of the trail, to the nearest city</p> -->
+			<?php if (isset($cityError) & $cityError == true) {
+				echo "<p class='warning'>Please include the location of the trail, to the nearest city.</p>";
+			} ?>
 		</fieldset>
 
 		<fieldset class="field short-text">
 			<legend class="large-voice">Why do you love this trail?</legend>
 			<input type="text" name="submissionReason">
-			<!-- <p class="warning">Filling in this field helps us decide what trails to curate</p> -->
+			<?php if (isset($reasonError) & $reasonError == true) {
+				echo "<p class='warning'>Filling in this field helps us decide what trails to curate</p>";
+			} ?>
 		</fieldset>
 
 		<fieldset class="field short-text">
-			<legend class="large-voice">Where can we find more about this trail?</legend>
+			<legend class="large-voice">Where can we learn about this trail?</legend>
 			<p class="medium-voice">Please share a link for us to review.</p>
 			<input type="text" name="submissionReference">
-			<!-- <p class="warning">Please include a link to the trail</p> -->
+			<?php if (isset($referenceError) & $referenceError == true) {
+				echo "<p class='warning'>Please include a link to the trail</p>";
+			} ?>
 		</fieldset>	
 
 		<div class="submit-container">
@@ -44,22 +79,13 @@
 		</div>
 		
 		<?php
-			if(isset($_POST["submitted"])) {
-				$submissionFields = [
-					$submissionName = $_POST["submissionName"],
-					$submissionCity =$_POST["submissionCity"],
-					$submissionReason =$_POST["submissionReason"],
-					$submissionReference =$_POST["submissionReference"]
-				];
-
-				if (strlen($submissionName) * strlen($submissionCity) * strlen($submissionReason) * strlen($submissionReference) == 0) {
-					echo "<p class='warning'>Please fill out all fields before submitting.</p>";
-				}
+			if (strlen($submissionName) * strlen($submissionCity) * strlen($submissionReason) * strlen($submissionReference) == 0) {
+				echo "<p class='warning'>Please fill out all fields before submitting.</p>";
+			} else {
+				echo "<p class='success'>Thanks for suggesting a new trail! Our team will review your submission to add to our curated list.</p>";
 			}
-
 		?>
 
-		<!-- <p class="success">Thanks for suggesting a new trail! Our team will review your submission to add to our curated list.</p>	 -->
 	</form>
 
 </section>
