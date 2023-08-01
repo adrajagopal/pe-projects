55e261b4acaebf4bf4d69c299620d0058dd99411
adrajagopal <adrajagopal@gmail.com>
2023-08-01 17:10:03 -0500

    Create ultra-layout-challenge header

---
+++ ultra-layout-challenge-2/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Layout Madness</title>
+	<?php include("../config.php"); ?>
+	<base href="<?=BASE_URL?>ultra-layout-challenge-2/">
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+	<div class="site-header inner-column">
+		<nav>
+			<a href="#" class="logo">logo</a>
+			<a href="#">Some page</a>
+			<a href="#">"Masthead"</a>
+		</nav>
+		<div class="sign-in">
+			<a href="#">Sign in</a>
+		</div>
+	</div>
+</header>
+
+<main class="page-wrapper">
