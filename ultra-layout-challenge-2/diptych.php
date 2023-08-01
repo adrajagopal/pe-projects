ccfbbe39492a3f0941ee5dea764b2384c125c921
adrajagopal <adrajagopal@gmail.com>
2023-08-01 17:10:45 -0500

    Create diptych section

---
+++ ultra-layout-challenge-2/diptych.php
@@ -0,0 +1,9 @@
+<section class="diptych">
+	<div class="inner-column">
+		<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
+		<div class="diptych-text">
+			<h1 class="xxl-voice">Hello! This is a 'diptych'</h1>
+			<p class="medium-voice">You can call it whatever you want. It's a module that has an image and supporting text. On the smaller screen it works nicely to stack. On a larger screen, it fits side by side.</p>
+		</div>
+	</div>
+</section>
