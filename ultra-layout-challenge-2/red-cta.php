834b9790ea30520524633628bca66e1b928d4a3d
adrajagopal <adrajagopal@gmail.com>
2023-08-01 22:26:24 -0500

    Create cta modules

---
+++ ultra-layout-challenge-2/red-cta.php
@@ -0,0 +1,7 @@
+<section class="red-cta">
+	<div class="inner-column">
+		<h2 class="xl-voice">This module is a "call to action"</h2>
+		<p class="medium-voice">These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern</p>
+		<a class="cta-button">HERE'S THE ACTION!</a>
+	</div>
+</section>
