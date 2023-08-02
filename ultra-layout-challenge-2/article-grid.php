70d008db09f2db95ca649a460adadcba70492b11
adrajagopal <adrajagopal@gmail.com>
2023-08-01 22:27:18 -0500

    Create article grid module (minus loops)

---
+++ ultra-layout-challenge-2/article-grid.php
@@ -0,0 +1,40 @@
+<section class="article-parent">
+	<div class="inner-column">
+		<div class="section-intro">
+			<h2 class="xl-voice">This is an "article grid" module. This is its heading.</h2>
+			<p class="medium-voice">Again, we totally just made that up. It's a box with boxes in it.</p>
+		</div>
+		<div class="article-grid">
+			<div class="article">
+				<h3 class="large-voice">Title</h3>
+				<p class="medium-voice">Quick description</p>
+				<a href="" class="cta-button">READ MORE</a>
+			</div>
+			<div class="article">
+				<h3 class="large-voice">Title</h3>
+				<p class="medium-voice">Quick description</p>
+				<a href="" class="cta-button">READ MORE</a>
+			</div>
+			<div class="article">
+				<h3 class="large-voice">Title</h3>
+				<p class="medium-voice">Quick description</p>
+				<a href="" class="cta-button">READ MORE</a>
+			</div>
+			<div class="article">
+				<h3 class="large-voice">Title</h3>
+				<p class="medium-voice">Quick description</p>
+				<a href="" class="cta-button">READ MORE</a>
+			</div>
+			<div class="article">
+				<h3 class="large-voice">Title</h3>
+				<p class="medium-voice">Quick description</p>
+				<a href="" class="cta-button">READ MORE</a>
+			</div>
+			<div class="article">
+				<h3 class="large-voice">Title</h3>
+				<p class="medium-voice">Quick description</p>
+				<a href="" class="cta-button">READ MORE</a>
+			</div>
+		</div>
+	</div>
+</section>
