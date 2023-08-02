a584d492ce53215a44ae4c55caac4381e0848128
adrajagopal <adrajagopal@gmail.com>
2023-08-02 11:27:57 -0500

    Move section/inner column from partials to index

--- ultra-layout-challenge-2/article-grid.php
+++ ultra-layout-challenge-2/article-grid.php
@@ -1,22 +1,20 @@
-<section class="article-parent">
-	<div class="inner-column">
-		<div class="section-intro">
-			<h2 class="xl-voice">This is an "article grid" module. This is its heading.</h2>
-			<p class="medium-voice">Again, we totally just made that up. It's a box with boxes in it.</p>
-		</div>
-		<ul class="article-grid">
-			<?php foreach ($articles as $article) {
-				$articleTitle = $article["title"];
-				$articleDesc = $article["description"];
-				$articleCtaCopy = $article["cta-copy"];
-				$articleLink = $article["episode-link"];
-				?>
-			<li class="article">
-				<h3 class="large-voice"><?=$articleTitle?></h3>
-				<p class="medium-voice"><?=$articleDesc?></p>
-				<a href="<?=$articleLink?>" class="cta-button"><?=$articleCtaCopy?></a>
-			</li>
-		<?php } ?>
-		</ul>
+<div class="article-grid">
+	<div class="section-intro">
+		<h2 class="xl-voice">This is an "article grid" module. This is its heading.</h2>
+		<p class="medium-voice">Again, we totally just made that up. It's a box with boxes in it.</p>
 	</div>
-</section>
+	<ul class="article-list">
+		<?php foreach ($articles as $article) {
+			$articleTitle = $article["title"];
+			$articleDesc = $article["description"];
+			$articleCtaCopy = $article["cta-copy"];
+			$articleLink = $article["episode-link"];
+			?>
+		<li class="article">
+			<h3 class="large-voice"><?=$articleTitle?></h3>
+			<p class="medium-voice"><?=$articleDesc?></p>
+			<a href="<?=$articleLink?>" class="cta-button"><?=$articleCtaCopy?></a>
+		</li>
+	<?php } ?>
+	</ul>
+</div>
--- ultra-layout-challenge-2/index.php
+++ ultra-layout-challenge-2/index.php
@@ -1,15 +1,32 @@
-<?php
-	include("header.php"); 
 
-	include("diptych.php");
+<?php	include("header.php");?>
 
-	include("purple-cta.php");
 
-	include("article-database.php");
+<section class="diptych">
+	<div class="inner-column">
+		<?php	include("diptych.php");?>
+	</div>
+</section>
 
-	include("article-grid.php");
+<section class="purple-cta">
+	<div class="inner-column">
+		<?php	include("purple-cta.php");?>
+	</div>
+</section>
 
-	include("red-cta.php");
+<?php	include("article-database.php");?>
 
-	include("footer.php");
-?>
+<section class="article-parent">
+	<div class="inner-column">
+		<?php	include("article-grid.php");?>
+	</div>
+</section>
+
+<section class="red-cta">
+	<div class="inner-column">
+		<?php	include("red-cta.php");?>
+	</div>
+</section>
+
+
+<?php	include("footer.php");?>
