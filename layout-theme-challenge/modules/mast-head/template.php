18b3cd442f756cb18e9ae614901008687c5ac4a2
adrajagopal <adrajagopal@gmail.com>
2023-08-05 10:18:59 -0500

    Upload layout challenge files

---
+++ layout-theme-challenge/modules/mast-head/template.php
@@ -0,0 +1,33 @@
+
+<mast-head>
+
+	<nav class='site-menu'>
+		<ul>
+			<li>
+				<a class='home' href='#'>
+					<?php include('modules/logo.php'); ?>
+				</a>
+			</li>
+
+			<li>
+				<a href='#'>Some page</a>
+			</li>
+
+			<li>
+				<a href='#'>Some other page</a>
+			</li>
+		</ul>
+	</nav>
+
+	<nav class='user-menu'>
+		<ul>
+			<li>
+				<a class='button' href='#'>Log in</a>
+			</li>
+			<li>
+				<a class='button' href='#'>Sign up</a>
+			</li>
+		</ul>
+	</nav>
+
+</mast-head>
