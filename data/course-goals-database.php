0eba6c52a986b5e0cdda5a302efd30f63d20a3bb
adrajagopal <adrajagopal@gmail.com>
2023-08-09 20:26:26 -0500

    Create databases for pages

---
+++ data/course-goals-database.php
@@ -0,0 +1,20 @@
+<?php
+	$courseGoals = [
+		[
+			"title" => "By the middle of the course...",
+			"description" => "I will be highly proficient in HTML and CSS, enough that I could take on contract/freelance work to supplement my learning, start to build a portfolio, and earn a small amount of money."
+		],
+		[
+			"title" => "By the end of the course...",
+			"description" => "I will be be proficient in HTML, CSS, JavaScript to a level where I could secure a full-time job as a frontend developer/frontend engineer/web developer."
+		],
+		[
+			"title" => "Two years from now...",
+			"description" => "I will be established in my career as a frontend developer. I will be gainfully employed, contributing at a high level individually, and working on something meaningful. I will be responsible for building beautiful websites and apps that perform well and make users happy. I will also have used my development skills to start to build independent income streams, work on a passion project, and enable a flexible lifestyle."
+		],
+		[
+			"title" => "When I retire...",
+			"description" => "I will have had a career with no big regrets. My career will have been stimulating and satisfying, without keeping me away from my family. I will have raised a child with my wife, loved my dogs, and spent ample time with family and friends. I will have kept my passions alive, stayed in shape (physically and mentally), and eaten and rested well."
+		],
+	]
+?>
---
+++ data/ideal-job-database.php
@@ -0,0 +1,44 @@
+<?php
+	$idealJobTraits = [
+		[
+			"title" => "Intellectual stimulation",
+			"description" => "I can’t do repetitive work, and I can’t do highly transactional work. I need to be stimulated"
+		],
+		[
+			"title" => "Boundaries and balance",
+			"description" => "I’m not great at setting boundaries, but it’s the most important thing for my happiness. If I don’t want to feel chained to a desk, I need to be in control of when and how I work"
+		],
+		[
+			"title" => "Making things",
+			"description" => "I need to make something. Physical, visual, digital, whatever. I like to create"
+		],
+		[
+			"title" => "Being in the weeds",
+			"description" => "I understand the importance of the “big picture”, and I can certainly operate at that level. But I need a job that’s skewed towards the little details, and being on the ground level"
+		],
+		[
+			"title" => "Fun",
+			"description" => "I’ve spent a lot of time working in healthcare, which is pretty serious work. It’s time for a change. I want to work on something not so serious!"
+		],
+		[
+			"title" => "Social good",
+			"description" => "I really want to emphasize fun, but I also need a mission to get behind. I haven't picked a specific direction yet, but I think playing music and spending time outdoors are probably at the top of my list. If I can enable either of those things, I’ll feel good about my role in the world"
+		],
+		[
+			"title" => "Steady growth",
+			"description" => "I want to work hard and learn, but I don’t ever want to feel burned out again. I’ll take steady growth over a constant grind to get where I’m going"
+		],
+		[
+			"title" => "A livable wage",
+			"description" => "If I’m not making enough money to support a modest lifestyle, the stress is going to force me to chase shiny things all over again"
+		],
+		[
+			"title" => "Health benefits",
+			"description" => "As the spouse of someone with a chronic illness, health insurance is not a luxury we can live without"
+		],
+		[
+			"title" => "Ownership",
+			"description" => "Finally, a buzzword!! But really, I just want to feel like I’m in control of my life and my work. And I want to own the benefits of my work. So many people feel like they work their entire careers for someone else, and they don’t have much to show for it. The money, the skills, the time — I don’t want to work for someone who takes all of that from me without giving back much to me"
+		]	
+	]
+?>
