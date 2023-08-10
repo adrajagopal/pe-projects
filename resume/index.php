0889e45b345394945ded3083d2058184bab85f86
adrajagopal <adrajagopal@gmail.com>
2023-08-09 20:26:13 -0500

    Modularize resume page

--- resume/index.php
+++ resume/index.php
@@ -1,66 +1,37 @@
+<head>
+	<link rel="stylesheet" href="../css/site.css"> 
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&family=Vidaloka&display=swap" rel="stylesheet">
+</head>
+
 <?php
-	include("../header.php");
-	include("../resume-job-list.php");
+	// include("../header.php");
+	include("../data/resume-job-list.php");
 ?>
 
-<?php $skills = ["Properly nested HTML", "CSS", "SQL"]; ?>
+<?php $skills = ["HTML", "CSS", "SQL", "PHP"]; ?>
 
+<section class="resume-intro">
+	<div class="inner-column">
+		<?php include("../modules/resume/resume-intro/template.php");?>
+	</div>
+</section>
 
-<h1 class="xxl-voice resume-title">Ad Rajagopal</h1>
+<section class="work-experience">
+	<div class="inner-column">
+		<?php include("../modules/resume/work-experience/template.php");?>
+	</div>
+</section>
 
-<p class="medium-voice">I'm a web developer. I'm coming to webdev with 7 years of experience in analytics, consulting and product management in the healthcare space. I've always worked on small-but-growing teams, whether that's deeply embedded in a corporation of 80,000 employees, or as the first IC product manager of a pre-seed startup. My former clients, colleagues, and managers would (and have!) described me as extremely organized, attentive to detail, proactive, brilliant, and a joy to be around. What will you say?</p>
+<section class="education-experience">
+	<div class="inner-column">
+		<?php include("../modules/resume/education-experience/template.php");?>
+	</div>
+</section>
 
-
-<!-- Work Experience section -->
-
-<h2 class="xl-voice">Work Experience</h2>
-
-<ul>
-
-<?php foreach ($jobs as $job) { 
-	$startDate = date_format($job["tenure"]["startDate"], "M Y");
-	$endDate = date_format($job["tenure"]["endDate"], "M Y");
-	$duration = date_diff($job["tenure"]["startDate"], $job["tenure"]["endDate"]);
-?>
-	
-	<li>
-		<h3 class="large-voice"><?=$job["role"]?> | <?=$job["organization"]?> |&nbsp;<?=$startDate?>&nbsp;-&nbsp;<?=$endDate?> </h3>
-		<ul>
-			<?php
-				foreach ($job["actions"] as $action) { ?>
-					<li class="job-bullet"><?=$action?></li>
-			<?php	} ?>
-		</ul>
-	</li>
-<?php } ?>
-
-</ul>
-
-<!-- Education section -->
-
-<h2 class="xl-voice">Education</h2>
-
-<h3 class="large-voice">Perpetual Education - Design for the Web</h3>
-
-<p class="medium-voice">Design for the Web, 2023</p>
-<p class="medium-voice">Not your average frontend bootcamp.</p>
-
-<h3 class="large-voice">University of Florida, Gainesville</h3>
-
-<p class="medium-voice">BA Economics, Cum Laude, 2015</p>
-
-<!-- Skill section -->
-
-<h2 class="xl-voice">Skills</h2>
-
-<ul class="medium-voice skill-list">
-	<?php
-		foreach ($skills as $skill) { 
-			echo "<li class='skill'>$skill</li>";
-		} 
-	?>
-</ul>
-
-
+<section class="skills-section">
+	<div class="inner-column">
+		<?php include("../modules/resume/skills-section/template.php");?>
+	</div>
+</section>
 
 <?php include("../footer.php");?>
