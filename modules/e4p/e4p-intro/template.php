6177078ace39967a1db43bf6021928dd41322be9
adrajagopal <adrajagopal@gmail.com>
2023-08-12 14:20:41 -0500

    Add copy to projects & e4p

--- modules/e4p/e4p-intro/template.php
+++ modules/e4p/e4p-intro/template.php
@@ -1,7 +1,11 @@
 <h3 class="large-voice">Exercises for Programmers</h3>
 
-<p class="medium-voice">
+<p>
 	<a href="https://pragprog.com/titles/bhwb/exercises-for-programmers/" target="_blank">Exercises for Programmers</a> is a book of more than 50 practice exercises with real applications. Here are a few I've done.
+</p>
+
+<p>
+	The purpose of these exercises is to think through real-world problems and practice working constraints, handling edge cases, and generally make good user experience decisions. It also tends to involve a lot of practice with forms...
 </p>
 
 <ul role="list" class="medium-voice bulleted-list">
--- projects/index.php
+++ projects/index.php
@@ -1,12 +1,29 @@
 <?php include("../header.php");?>
 
+<section class="welcome">
+	<div class="inner-column">
+		<h1 class="xxl-voice">Projects</h1>
 
-<h1 class="xxl-voice">Projects</h1>
+		<h2 class="xl-voice">Here's a selection of projects.</h2>
+	</div>
+</section>
 
-<h2 class="xl-voice">Here's a selection of projects.</h2>
+<section class="theme-challenges-intro">
+	<div class="inner-column">
+		<h3 class="large-voice">Theme & layout projects</h3>
+
+		<p>These projects are heavily focused on CSS. Each project covers the basics - margins, padding, image sizing, color/contrast and typography - and then goes all the way into responsive design using grid, flexbox, and positioning. In the middle, there are the little details.</p>
+
+		<ul>
+			<li>
+				<p><a href="projects/layout-theme-challenge" target="_blank">Layout Theme Challenge</a> - a demonstration of how far CSS can take you. Check out my <a href="#" target="_blank">case study</a> to see how I spent my week exploring & recreating digital health companies.
+			</li>
+		</ul>
+	</div>
+</section>
 
 <section class="e4p-intro">
-	<div class="inner-column"></div>
+	<div class="inner-column">
 		<?php include(getFile('modules/e4p/e4p-intro/template.php'));?>
 	</div>
 </section>
