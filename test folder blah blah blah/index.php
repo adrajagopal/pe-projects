176c515d61e30ad1c307fe2938411da9f2672925
adrajagopal <adrajagopal@gmail.com>
2023-08-15 13:10:34 -0500

    Test
    test
    
    undo test

---
+++ test folder blah blah blah/index.php
@@ -0,0 +1,17 @@
+<?php include('header.php');?>
+
+<h1 class="xxl-voice inner-column">Hiiiiiii</h1>
+
+<section class="about-me">
+	<div class="inner-column">
+		<?php include("modules/about-me/template.php");?>
+	</div>
+</section>
+
+<section class="latest-projects">
+	<div class="inner-column">
+		<?php include("modules/latest-projects/template.php");?>
+	</div>
+</section>
+
+<?php include("site-footer.php");?>
