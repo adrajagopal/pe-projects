ba0567aaca4e5ca68f63e8e0c00ee02e9a38262a
adrajagopal <adrajagopal@gmail.com>
2023-08-16 17:43:10 -0500

    Add colored squares to diptych grid

--- layout-garden-modules/helfy-diptych/template.php
+++ layout-garden-modules/helfy-diptych/template.php
@@ -1,16 +1,20 @@
 
 <graphic-diptych>
 
+	<div class="box1"></div>
+	<div class="box2"></div>
+	<div class="box3"></div>
+
 	<picture>
 		<img src="layout-garden-modules/helfy-diptych/images/patient-green-background.jpg" alt='female patient smiling'>
 	</picture>
 	
 	<text-content>
-		<h1 class='loud-voice'>Helfy. Your <em>fake</em> digital health company.</h1>
+		<h1 class='xl-voice'>Helfy. Your <em>fake</em> digital health company.</h1>
 
 		<p>Here's a description to go in the diptych</p>
 		
-		<a href="" class="button">Join now</a>
+		<a href="" class="button medium-voice">Join now</a>
 
 	</text-content>
 
--- layout-garden/index.php
+++ layout-garden/index.php
@@ -1,30 +1,30 @@
 <?php include('../header.php');?>
 
-<section class="layout-garden-heading">
+<section class="layout-garden-heading" style="margin-top: 5vh;">
 	<div class="inner-column">
 		<?php include(getFile('layout-garden-modules/page-heading/template.php')); ?>
 	</div>
 </section>
 
-<section class="diptych-intro">
+<section class="diptych-intro" style="margin-top: 5vh;">
 	<div class="inner-column">
 		<h2 class="xl-voice">Graphic Diptych/Hero/Welcome Sections</h2>
 	</div>
 </section>
 
-<section class="helfy-diptych">
+<section class="helfy-diptych" style="margin-top: 5vh;">
 	<div class="inner-column">
 		<?php include(getFile('layout-garden-modules/helfy-diptych/template.php')); ?>
 	</div>
 </section>
 
-<section class="helfy-diptych secondary">
+<section class="helfy-diptych secondary" style="margin-top: 5vh;">
 	<div class="inner-column">
 		<?php include(getFile('layout-garden-modules/helfy-diptych/template.php')); ?>
 	</div>
 </section>
 
-<section class="imessage-diptych">
+<section class="imessage-diptych" style="margin-top: 5vh;">
 	<div class="inner-column">
 		<?php include(getFile('layout-garden-modules/imessage-diptych/template.php')); ?>
 	</div>
