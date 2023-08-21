c2ca69706815f26a1646fb37dca6d85ac58d8db6
adrajagopal <adrajagopal@gmail.com>
2023-08-21 17:15:59 -0500

    Modularize E4P and theme challenge
    Mod

---
+++ modules/theme-challenges-intro/template.php
@@ -0,0 +1,15 @@
+<h3 class="large-voice">Theme & layout projects</h3>
+
+<p>These projects are heavily focused on CSS. Each project covers the basics - margins, padding, image sizing, color/contrast and typography - and then goes all the way into responsive design using grid, flexbox, and positioning. In the middle, there are the little details.</p>
+
+<ul role="list" class="bulleted-list">
+	<li>
+		<p><a href="projects/layout-theme-challenge" target="_blank">Layout Theme Challenge</a> - a demonstration of how far CSS can take you. Check out my <a href="#" target="_blank">case study</a> to see how I spent my week exploring & recreating digital health companies.
+	</li>
+	<li>
+		<p><a href="projects/layout-garden">Responsive Layout Garden</a> - a gallery of responsive modules for pretty much anything you would stick on a website.
+	</li>
+	<li>
+		<p><a href="projects/style-guide">Style Guide</a> - style guides are used to show how a design system would look for a given set of content. This includes typography (font sizes, weights, styles; letter spacing and line height; "voicings"), colors & contrast, component styling, and content spacing. The more content, the more comprehensive the guide.
+	</li>
+</ul>
