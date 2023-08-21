4803ae966b3c6466fc6865abe93cb1676239b96b
adrajagopal <adrajagopal@gmail.com>
2023-08-21 17:14:42 -0500

    Add routing to about page

--- about/index.php
+++ about/index.php
@@ -1,26 +1,24 @@
 <?php include('../header.php');?>
 
-<?php include(getFile('modules/about-nav/template.php'));?>
-
 <?php
 
-	$page="";
+	$page="goals";
 
 	if ( isset($_GET['page'])) {
 		$page = $_GET['page'];		
 	}
+?>
+
+<?php
+	include(getFile('modules/about-nav/template.php'));
+
 
 	if ($page === "goals") {
 		include(getFile('modules/goals/index.php'));
 	} elseif ($page === "resume") {
 		include(getFile('modules/resume/index.php'));
-	} else {
-		echo "<p class='xxl-voice'>not sure what to do here yet</p>";
 	}
-
 ?>
-
-
 
 <?php
 	include(getFile('site-footer.php'));
--- modules/about-nav/template.php
+++ modules/about-nav/template.php
@@ -1,8 +1,8 @@
 <section class="about-header">
 	<div class="inner-column">
 		<nav class="about-nav">
-			<a class="<?php if ($_GET['page'] === "goals") {echo "active-page";}?>" href="about?page=goals">Goals</a>
-			<a class="<?php if ($_GET['page'] === "resume") {echo "active-page";}?>" href="about?page=resume">Resume</a>
+			<a class="<?php if ($page === 'goals') {echo 'active-page';}?>" href="about?page=goals">Goals</a>
+			<a class="<?php if ($page === 'resume') {echo 'active-page';}?>" href="about?page=resume">Resume</a>
 		</nav>
 	</div>
 </section>
