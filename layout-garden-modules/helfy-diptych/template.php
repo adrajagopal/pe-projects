ac285367848e88a8e9baa8518304da1eba72dad0
adrajagopal <adrajagopal@gmail.com>
2023-08-22 21:47:39 -0500

    Update layout garden helfy diptych styling

--- layout-garden-modules/helfy-diptych/template.php
+++ layout-garden-modules/helfy-diptych/template.php
@@ -1,20 +1,19 @@
-
-<graphic-diptych>
+<graphic-diptych class="layout-garden">
 
 	<div class="box1"></div>
 	<div class="box2"></div>
 	<div class="box3"></div>
 
 	<picture>
-		<img src="layout-garden-modules/helfy-diptych/images/patient-green-background.jpg" alt='female patient smiling'>
+		<img src="layout-garden-modules/helfy-diptych/images/patient-green-background.jpg" alt='imaginary customer smiling and looking off into the distance, immensely happy that their identity is tied so closely to the brand identity'>
 	</picture>
 	
 	<text-content>
-		<h1 class='xl-voice'>Helfy. Your <em>fake</em> digital health company.</h1>
+		<h1 class='xl-voice'>Witty slogan. Here are <em>words</em> to catch your eye.</h1>
 
-		<p>Here's a description to go in the diptych</p>
+		<p>An additional selling point to add some more detail.</p>
 		
-		<a href="" class="button medium-voice">Join now</a>
+		<a href="" class="button medium-voice">Take action</a>
 
 	</text-content>
 
