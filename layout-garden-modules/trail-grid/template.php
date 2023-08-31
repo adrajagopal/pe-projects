e431349abf1ae6aba45a3d4c4a54a90104831736
adrajagopal <adrajagopal@gmail.com>
2023-08-31 09:50:45 -0500

    Add overlay to biggest breakpoint

--- layout-garden-modules/trail-grid/template.php
+++ layout-garden-modules/trail-grid/template.php
@@ -14,27 +14,27 @@
 
 <h2 class="xl-voice">Trending Trails</h2>
 
-<!-- <div class="split-content"> -->
+<ul class="trail-list">
+	<?php foreach ($trails as $trail) {
+		$title = $trail['title'];
+		$length = $trail['length'];
+		$time = $trail['time'];
+		$portrait = $trail['portrait']; ?>
+		
+		<li>
+			<picture><img src="<?=$portrait?>"></picture>
+			<h3 class="medium-voice"><?=$title?></h3>
+			<p>Length: <?=$length?> • Est. <?=$time?></p>
+		</li>
+	<?php } ?>
+</ul>
 
-	<ul class="trail-list">
-		<?php foreach ($trails as $trail) {
-			$title = $trail['title'];
-			$length = $trail['length'];
-			$time = $trail['time'];
-			$portrait = $trail['portrait']; ?>
-			
-			<li>
-				<picture><img src="<?=$portrait?>"></picture>
-				<h3 class="medium-voice"><?=$title?></h3>
-				<p>Length: <?=$length?> • Est. <?=$time?></p>
-			</li>
-		<?php } ?>
-	</ul>
-
-	<div class="see-more">
+<div class="see-more">
+	<a href="projects/layout-garden#:~:text=Trending-,Trails,-Meyers%20Beach%20Sea">
 		<h3 class="large-voice">See more trails</h3>
 		<svg class="icon-"><use xlink:href="#icon-chevron-right"></use></svg>
-	</div>
-<!-- </div> -->
+	</a>
+</div>
+
 
 </section>
