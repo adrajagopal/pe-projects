b92736647f064d0783ce2b60b3e505e84ef1f554
adrajagopal <adrajagopal@gmail.com>
2023-09-01 19:49:36 -0500

    Create compound-interest-form

--- data/e4p-list.php
+++ data/e4p-list.php
@@ -10,6 +10,11 @@ $e4pExercises = [
 		"name" => "Paint Calculator",
 		"description" => "Calculates the number of gallons, rounded up, needed to paint a rectangular room.",
 		"link" => "paint-calculator"
+	],
+	$e4pCompoundInterest = [
+		"name" => "Compound Interest Calculator",
+		"description" => "Calculates the amount a principal will grow to, given a compounding schedule and duration",
+		"link" => "compound-interest"
 	]
 ];
 
