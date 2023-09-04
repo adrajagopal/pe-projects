38c1aca934f17b0f96cd8fffdfd101a0ee0bd1b1
adrajagopal <adrajagopal@gmail.com>
2023-09-04 14:26:36 -0500

    Write function for getPage

--- functions.php
+++ functions.php
@@ -16,12 +16,28 @@ function showErrors() {
 
 showErrors();
 
-
 //Note - this function calculates the dirname based on where this function lives in the directory - probably??
 function getFile($path) {
 	return dirname(__FILE__) . '/' . $path;
 }
 
 
+//checks if current page is active page - use to output class for CSS
+function ifActivePage($pageName) {
+	$page = "goals";
+
+	if (isset($_GET['page'])) {
+		$page = $_GET['page'];				
+	}
+
+	if ($page === $pageName) {
+			return 'active-page';
+	}
+}
+
+//returns the query string behind the ?
+function queryString() {
+	return $_SERVER['QUERY_STRING'];
+}
 
 
--- modules/about-nav/template.php
+++ modules/about-nav/template.php
@@ -1,8 +1,8 @@
 <section class="about-header">
 	<inner-column">
 		<nav class="about-nav">
-			<a class="<?php if ($page === 'goals') {echo 'active-page';}?>" href="about?page=goals">Goals</a>
-			<a class="<?php if ($page === 'resume') {echo 'active-page';}?>" href="about?page=resume">Resume</a>
+			<a class="<?=ifActivePage('goals')?>" href="about?page=goals">Goals</a>
+			<a class="<?=ifActivePage('resume')?>" href="about?page=resume">Resume</a>
 		</nav>
 	</inner-column>
 </section>
