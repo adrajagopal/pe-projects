1e8c857a63fc615f41dc4466b97b0c127c2b30cb
adrajagopal <adrajagopal@gmail.com>
2023-09-05 16:02:20 -0500

    Create templates
    Create new templates

---
+++ mockup-to-code-challenge/components/email-subscribe/template.php
@@ -0,0 +1,6 @@
+<form>
+	<label for="">
+		<input type="text" placeholder="Email address">
+	</label>
+	<button>></button>
+</form>
---
+++ mockup-to-code-challenge/modules/hero/template.php
@@ -0,0 +1,13 @@
+<div class="hero-container">
+	
+	<text-content>
+		<h1 class="xxl-voice">Arcu Suscipit Massa Aliquam Proin Amet</h1>
+		<p>Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse luctus Amet bibendum pellentesque lorem. Malesuada lobortis tristique tortor.</p>
+	</text-content>
+	<div class="links-container">
+		<a class="button">Get Started</a>
+		<a class="button">Start Free Trial</a>
+		<?php include(getFile('components/email-subscribe/template.php')) ; ?>
+	</div>
+
+</div>
