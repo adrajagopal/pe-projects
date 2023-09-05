d326c03a496eebf7dae44ac99d9ddc0591e22d53
adrajagopal <adrajagopal@gmail.com>
2023-09-04 23:05:50 -0500

    Create mockup challenge folder & initial structure

---
+++ mockup-to-code-challenge/header.php
@@ -0,0 +1,73 @@
+<?php include('config.php');?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<base href="<?=BASE_URL?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+
+<!-- delete this comment once you've replaced the template content -->
+	<title>Pixel Perfect Challenge</title>
+
+<!-- delete this comment once you've replaced the template content -->
+	<meta name="description" content="This is some page. You hit it.">
+	
+<!-- delete this comment once you've replaced the template content -->
+	<meta property="og:title" content="Ad's Portfolio'">
+
+<!-- delete this comment once you've replaced the template content -->
+	<!-- <meta
+	  property="og:description"
+	  content="[[INSERT CONTENT]]"> -->
+
+<!-- delete this comment once you've replaced the template content -->
+	<meta
+	  property="og:image"
+	  content="[[INSERT CONTENT]].png">
+	
+	<link rel="stylesheet" href="css/site.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+	
+<?php include('../icons.php');?>
+
+<header class="site-header" aria-label="Site navigation menu">
+	<inner-column>
+		<div class="header-items">			
+			<a href="" class="header-logo">
+				<svg viewBox='0 0 10 3'>
+					<rect width='10' height='3' />
+				</svg>
+			</a>
+
+			<nav class="site-nav">
+				<ul class="site-nav-items">
+					<li><a href="">Home</a></li>
+					<li><a href="">Updates</a></li>
+					<li><a href="">Services</a></li>
+					<li><a href="">Features</a></li>
+					<li><a href="">About Us</a></li>
+				</ul>
+			</nav>
+
+			<div class="login-items">
+				<a href="" class="language-indicator">
+					<svg viewBox='0 0 10 3'>
+						<rect width='10' height='3' />
+					</svg>
+				</a>
+				<a href="" class="login">Log in</a>
+				<a href="" class="signup">Sign up</a>
+			</div>
+		</div>
+	</inner-column>
+</header>
+
+	<main class="page-wrapper">
---
+++ mockup-to-code-challenge/site-footer.php
@@ -0,0 +1,17 @@
+	</main>
+
+<!-- Site footer-------------------------------------- -->
+	
+	<footer class="site-footer" aria-label="Site footer">
+		<inner-column>
+	
+		<div class="footer-list">
+			<?php include('../socials.php');?>
+		</div>
+		
+		</inner-column>		
+	</footer>
+
+
+</body>
+</html>
