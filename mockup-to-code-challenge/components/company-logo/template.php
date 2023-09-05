f1de555a731124dcfb2e43355eeec854413ec1df
adrajagopal <adrajagopal@gmail.com>
2023-09-05 16:56:17 -0500

    Create components for logo and icon placeholders

---
+++ mockup-to-code-challenge/components/company-logo/template.php
@@ -0,0 +1,5 @@
+<a href="" class="company-logo">
+	<svg viewBox='0 0 10 3'>
+		<rect width='10' height='3' />
+	</svg>
+</a>
---
+++ mockup-to-code-challenge/components/icon-placeholder/template.php
@@ -0,0 +1,5 @@
+<div class="icon-placeholder">
+	<svg viewBox='0 0 10 10'>
+		<circle cx='5' cy='5' r='5' />
+	</svg>
+</div>
