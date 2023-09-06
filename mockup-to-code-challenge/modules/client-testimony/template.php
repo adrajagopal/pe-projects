5d8b4852049ecacafb6297d1cbf1da044b4f6244
adrajagopal <adrajagopal@gmail.com>
2023-09-06 10:08:24 -0500

    Create client testimony module

--- mockup-to-code-challenge/modules/client-testimony/template.php
+++ mockup-to-code-challenge/modules/client-testimony/template.php
@@ -1,1 +1,28 @@
-index.php
+<div class="client-testimony-container">
+	<text-content>
+		<h2 class="xl-voice">What Our Clients Say</h2>
+		<p class="small-voice">Neque, Pulvinar Vestibulum Non Aliquam</p>	
+	</text-content>
+	
+
+	<carousel>
+		<div class="left-arrow"><?php include(getFile('components/icon-placeholder/template.php'));?></div>
+		<ul>
+			<?php
+				$json = file_get_contents(getFile('data/client-feedback.json'));
+		
+				$quotes = json_decode($json, true);
+		
+				foreach ($quotes as $quote) { ?>
+		
+					<li>
+						<?php include(getFile('modules/quote-card/template.php'));?>
+					</li>
+		
+			<?php } ?>
+		</ul>
+		<div class="right-arrow"><?php include(getFile('components/icon-placeholder/template.php'));?></div>
+	</carousel>
+	
+	<picture><img src="https://peprojects.dev/images/landscape.jpg" alt=""></picture>
+</div>
---
+++ mockup-to-code-challenge/modules/quote-card/template.php
@@ -0,0 +1,15 @@
+<quote-card>
+	<div class="stars">
+		<?php
+			include(getFile('components/icon-placeholder/template.php'));
+			include(getFile('components/icon-placeholder/template.php'));
+			include(getFile('components/icon-placeholder/template.php'));
+			include(getFile('components/icon-placeholder/template.php'));
+			include(getFile('components/icon-placeholder/template.php'));
+		?>
+	</div>
+	<p><?=$quote['quote']?></p>
+	<p><?=$quote['name']?></p>
+	<p><?=$quote['role']?></p>
+	<?php include(getFile('components/icon-placeholder/template.php')); ?>
+</quote-card>
