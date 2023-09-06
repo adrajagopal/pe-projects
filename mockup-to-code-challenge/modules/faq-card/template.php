183e13a008c074b85ab2e9752eb01aad0db6229b
adrajagopal <adrajagopal@gmail.com>
2023-09-06 10:07:43 -0500

    Create FAQ section
    fq

---
+++ mockup-to-code-challenge/modules/faq-card/template.php
@@ -0,0 +1,7 @@
+<details class="faq-card">
+	<summary class="medium-voice">
+		<?=$faq['question']?>
+		<span class="dropdown">+</span>
+	</summary>
+	<p class="small-voice"><?=$faq['answer']?></p>
+</details>
