416f7f24afce931d0258fec3b410402c0ee9a863
adrajagopal <adrajagopal@gmail.com>
2023-09-11 10:09:29 -0500

    Finish CTA section layout across all templates

--- mockup-to-code-challenge/modules/call-to-action/template.php
+++ mockup-to-code-challenge/modules/call-to-action/template.php
@@ -1,22 +1,24 @@
 <div class="call-to-action-container">
-	<text-content>
-
-		<?php if(isset($section['content']['preheading'])) { ?>
-			<p class="preheading small-voice"><?=$section['content']['preheading']?></p>
-		<?php } ?>
+	<div class="content-container">
+		<text-content>
 		
-		<h2 class="xl-voice"><?=$section['content']['heading']?></h2>
+			<?php if(isset($section['content']['preheading'])) { ?>
+				<p class="preheading small-voice"><?=$section['content']['preheading']?></p>
+			<?php } ?>
+			
+			<h2><?=$section['content']['heading']?></h2>
+			
+			<?php if(isset($section['content']['description'])) { ?>
+				<p class=""><?=$section['content']['description']?></p>
+			<?php } ?>
 		
-		<?php if(isset($section['content']['description'])) { ?>
-			<p class=""><?=$section['content']['description']?></p>
-		<?php } ?>
-
-	</text-content>
-
-	<div class="links-container">
-		<?php foreach ($section['content']['actions'] as $action) {
-			include(getFile("components/$action[type]/template.php"));
-		} ?>
+		</text-content>
+		
+		<div class="links-container">
+			<?php foreach ($section['content']['actions'] as $action) {
+				include(getFile("components/$action[type]/template.php"));
+			} ?>
+		</div>
 	</div>
 
 	<?php if($page === "blue") { ?>
