3d23789780cbb56139def4c1f860a15255ae64be
adrajagopal <adrajagopal@gmail.com>
2023-09-10 20:35:42 -0500

    Update links-container

--- mockup-to-code-challenge/components/link/template.php
+++ mockup-to-code-challenge/components/link/template.php
@@ -2,8 +2,10 @@
 	<a href="<?=$action['link'] ?? "#"?>">
 		<span><?=$action['copy'] ?? "text"?></span>
 
-		<?php if (isset($action['icon'])) {
-			include(getFile("images/$action[icon].svg"));
-		} ?>
+		<div class="icon-container">
+			<?php if (isset($action['icon'])) {
+				include(getFile("images/$action[icon].svg"));
+			} ?>
+		</div>
 	</a>
 </div>
