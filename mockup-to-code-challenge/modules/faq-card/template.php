34ef70a8009fc4d8e8f7e5ca7e97259894f86df0
adrajagopal <adrajagopal@gmail.com>
2023-09-12 13:18:51 -0500

    FAQ layout styling - all 3 templates - HELP

--- mockup-to-code-challenge/modules/faq-card/template.php
+++ mockup-to-code-challenge/modules/faq-card/template.php
@@ -1,7 +1,21 @@
-<details class="faq-card">
-	<summary class="medium-voice">
-		<?=$faq['question']?>
-		<span class="dropdown">+</span>
-	</summary>
-	<p class="small-voice"><?=$faq['answer']?></p>
-</details>
+<?php 
+
+	$faqIcon = "chevron-right";
+
+	if ($page === "home" OR $page === "blue") {
+		$faqIcon = "chevron-right";		
+	} elseif ($page === "wavy") {
+		$faqIcon = "plus";
+	}
+
+?>
+
+<faq-card>
+	<details class="faq-card">
+		<summary class="medium-voice">
+			<?=$faq['question']?>
+			<div class="icon-container"><?php include(getFile("images/$faqIcon.svg")); ?></div>
+		</summary>
+		<p class="small-voice"><?=$faq['answer']?></p>
+	</details>
+</faq-card>
