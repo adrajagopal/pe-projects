2e5452a5c990fe897cdfa1315d263071b858de60
adrajagopal <adrajagopal@gmail.com>
2023-09-24 13:53:18 -0500

    Create color picker form template

--- modules/form-color-picker/script-min.js
+++ modules/form-color-picker/script-min.js
@@ -1,1 +1,1 @@
-console.log("hi");
+const $form=document.querySelector("form.colorPicker");
--- modules/form-color-picker/template.php
+++ modules/form-color-picker/template.php
@@ -1,12 +1,10 @@
-<?php include('../../header.php'); ?>
-
 <form class="colorPicker">
 
 	<label class="field slider span-two">
 		<span class="label">Hue</span>
 		<input-container>
 			<input id="hue" type="range" min="0" max="360" step="10">
-			<label for="">0-360, incr = 10</label>
+			<label for="hue">0-360, incr = 10</label>
 		</input-container>
 	</label>
 
@@ -14,7 +12,7 @@
 		<span class="label">Saturation</span>
 		<input-container>
 			<input id="saturation" type="range" min="0" max="100" step="1">
-			<label for="">0-100, incr = 1</label>
+			<label for="saturation">0-100, incr = 1</label>
 		</input-container>
 	</label>
 
@@ -22,7 +20,7 @@
 		<span class="label">Lightness</span>
 		<input-container>
 			<input id="lightness" type="range" min="0" max="100" step="1">
-			<label for="">0-100, incr = 1</label>
+			<label for="lightness">0-100, incr = 1</label>
 		</input-container>
 	</label>
 
