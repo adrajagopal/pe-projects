3df1ae6679386cbbc7d7dd6f0551eb5dc4bbcb24
adrajagopal <adrajagopal@gmail.com>
2023-09-26 21:49:46 -0500

    Cleanup

--- modules/welcome/template.php
+++
@@ -1,6 +0,0 @@
-<diptych class="diptych" style="display: block;">
-	<h1 class="xl-voice">I'm Ad. A product manager turned web developer.</h1>
-	<picture class="portrait">
-		<img src="images/ad-portrait.png" alt="a picture of me frowning at the camera">
-	</picture>
-</diptych>
