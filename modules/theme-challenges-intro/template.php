1e55bf3bb91f34c7c579d601ce12e507a3cd6270
adrajagopal <adrajagopal@gmail.com>
2023-09-28 10:18:53 -0500

    Add figma challenge projects page list

--- modules/theme-challenges-intro/template.php
+++ modules/theme-challenges-intro/template.php
@@ -4,7 +4,12 @@
 
 <ul role="list" class="bulleted-list">
 	<li>
-		<p><a href="projects/layout-theme-challenge" target="_blank">Layout Theme Challenge</a> - a demonstration of how far CSS can take you. Check out my <a href="#" target="_blank">case study</a> to see how I spent my week exploring & recreating digital health companies.
+		<p><a href="projects/layout-theme-challenge" target="_blank">Layout Theme Challenge</a> - a demonstration of how far CSS can take you. Check out my <a href="https://adrajagopal.substack.com/p/case-study-theme-challenge" target="_blank">case study</a> to see how I spent my week exploring & recreating digital health companies.
+	</li>
+	<li>
+		<p><a href="projects/mockup-to-code-challenge">Figma to Code Challenge</a> - a one-week sprint to prove that I can take a figma file and build 3 responsive landing pages while matching the design perfectly - spoiler, I only matched the pieces I agreed with. I pushed back on elements of the design that I couldn't complete in the allotted time, or didn't make sense when you added in a responsive layout, or just didn't look good once implemented. It was also an exercise in using JSON to drive templates and page content. Here's the original <a href="https://www.figma.com/file/TN28dkJ1pQ04fRlb2mqAqm/Wireframe-Kit?node-id=0:1&mode=dev" target="_blank">Figma file</a>.
+
+		<p class="small-voice">PS - click the links in the header to switch between layouts</p>
 	</li>
 	<li>
 		<p><a href="projects/layout-garden">Responsive Layout Garden</a> - a gallery of responsive modules for pretty much anything you would stick on a website.
