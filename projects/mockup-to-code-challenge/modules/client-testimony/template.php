57204921331532944eadc911b2c40297119d52ad
adrajagopal <adrajagopal@gmail.com>
2023-09-29 20:12:01 -0500

    Add flickity carousel across all 3 templates

--- projects/mockup-to-code-challenge/modules/client-testimony/template.php
+++ projects/mockup-to-code-challenge/modules/client-testimony/template.php
@@ -7,24 +7,27 @@
 	<?php if ($page === "blue") { ?>
 			<picture><img src="https://peprojects.dev/images/landscape.jpg" alt=""></picture>
 	<?php } ?>
-
-	<carousel>
-		<div class="carousel-arrow"><?php include(getFile('images/chevron-left.svg'));?></div>
-		<ul>
-			<?php
-				$json = file_get_contents(getFile('data/ratings.json'));
-		
-				$quotes = json_decode($json, true);
-		
-				foreach ($quotes as $quote) { ?>
-		
-					<li>
-						<?php include(getFile('modules/quote-card/template.php'));?>
-					</li>
-		
-			<?php } ?>
-		</ul>
-		<div class="carousel-arrow"><?php include(getFile('images/chevron-right.svg'));?></div>
-	</carousel>
+	
+	<ul
+		class="main-carousel"
+		<?php if ($page === "home" OR $page === "wavy") {
+			echo "data-flickity='{ \"cellAlign\": \"left\", \"contain\": true, \"wrapAround\": true }'";
+		} elseif ($page === "blue") {
+			echo "data-flickity='{ \"cellAlign\": \"left\", \"contain\": true, \"wrapAround\": true, \"prevNextButtons\": false, \"setGallerySize\": \"false\" }'";
+		}?>
+	>
+		<?php
+			$json = file_get_contents(getFile('data/ratings.json'));
+	
+			$quotes = json_decode($json, true);
+	
+			foreach ($quotes as $quote) { ?>
+	
+				<li class="carousel-cell">
+					<?php include(getFile('modules/quote-card/template.php'));?>
+				</li>
+	
+		<?php } ?>
+	</ul>
 	
 </div>
