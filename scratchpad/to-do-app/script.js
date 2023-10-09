fa1910e457f62fef9ef6acb71ed678ac8104f0c0
adrajagopal <adrajagopal@gmail.com>
2023-10-09 13:30:18 -0500

    Add showTweet functionality

--- scratchpad/to-do-app/script.js
+++ scratchpad/to-do-app/script.js
@@ -19,6 +19,42 @@ function addTweet() {
 	contentInput.value = "";
 }
 
+function showTweet(tweet) {
+	return `
+		<li data-id="${tweet.id}">
+			<tweet-card class="${ tweet.flagged ? 'flagged' : ''}">
+				<picture class="profile-photo">
+					<img src="images/${tweet.profilePic}" alt="">
+				</picture>
+
+				<div class="identity">
+					<p class="author">${tweet.username}</p>
+					<p class="handle">@${tweet.handle}</p>
+				</div>
+
+				<p class="tweet-body">${tweet.content}</p>
+
+				<div class="actions">
+					<button class="hide">
+						<picture>
+							<img src="images/hide-content.svg" alt="Hide tweet">
+						</picture>
+					</button>
+					<button class="flag">
+						<picture>
+							<img src="images/report-content.svg" alt="Flag tweet">
+						</picture>
+					</button>
+					<button class="dox">
+						<picture>
+							<img src="images/dox.svg" alt="Dox author">
+						</picture>
+					</button>
+				</div>
+			</tweet-card>
+		</li>
+	`;
+}
 const form = document.querySelector('form');
 const output = document.querySelector('output');
 
@@ -26,7 +62,6 @@ var usernameInput = form.querySelector('#username');
 var handleInput = form.querySelector('#handle');
 var profilePicInput = form.querySelector('#profile-pic');
 var contentInput = form.querySelector('#content');
-
 let username = null;
 let handle = null;
 let profilePic = null;
