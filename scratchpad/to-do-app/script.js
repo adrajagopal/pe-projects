f228627747b033c5ab79d2ccc4fe10c0818777d1
adrajagopal <adrajagopal@gmail.com>
2023-10-09 13:31:41 -0500

    Add hide, flag, dox functionality to tweets

--- scratchpad/to-do-app/script.js
+++ scratchpad/to-do-app/script.js
@@ -71,6 +71,36 @@ function showFeed() {
 
 }
 
+function hideTweet(id) {
+	const filtered = tweets.filter( function (tweet) {
+		return tweet.id != id;
+	});
+
+	tweets = [...filtered];
+	showFeed();
+
+}
+
+function flagTweet(id) {
+	const flaggedTweet = tweets.find( function(tweet) {
+		return tweet.id === id;
+	});
+
+	flaggedTweet.flagged = !flaggedTweet.flagged;
+
+	showFeed();
+
+}
+
+function doxUser(id) {
+	const found = tweets.find( function(tweet) {
+		return tweet.id === id;
+	});
+
+	alert(`The author of this tweet is ${found.username}. FUCK UP THEIR LIFE!!!!`);
+}
+
+
 const form = document.querySelector('form');
 const output = document.querySelector('output');
 
@@ -78,6 +108,7 @@ var usernameInput = form.querySelector('#username');
 var handleInput = form.querySelector('#handle');
 var profilePicInput = form.querySelector('#profile-pic');
 var contentInput = form.querySelector('#content');
+
 let username = null;
 let handle = null;
 let profilePic = null;
@@ -85,6 +116,7 @@ let content = null;
 
 form.addEventListener('submit', function(event) {
 	event.preventDefault();
+
 	username = usernameInput.value;
 	handle = handleInput.value;
 	profilePic = profilePicInput.value;
@@ -101,3 +133,30 @@ form.addEventListener('submit', function(event) {
 		alert("Please fill out all the fields");
 	}
 });
+
+output.addEventListener('click', function(event) {
+
+	if (event.target.className === 'hide') {
+		const id = event.target.closest('li').dataset.id;
+		hideTweet(id);
+	}
+
+	if (event.target.className === 'dox') {
+		const id = event.target.closest('li').dataset.id;
+		doxUser(id);
+	}
+
+	if (event.target.className === 'flag') {
+		const id = event.target.closest('li').dataset.id;
+		flagTweet(id);
+	}
+
+
+
+
+
+
+
+
+
+})
