083974bdece4c6b48c729136c4f775bc124b0008
adrajagopal <adrajagopal@gmail.com>
2023-10-14 14:32:14 -0500

    Build handleSignIn(), checkUsers(), render homepg

--- projects/goals-app/script.js
+++ projects/goals-app/script.js
@@ -1,4 +1,10 @@
+// window.localStorage.clear();
+
 var database = window.localStorage;
+
+//*************************** FUNCTIONS
+
+///////*************************** DOCUMENT STRUCTURE
 
 function createElementVar(elementName) {
 	return document.querySelector(`${elementName}`);
@@ -8,12 +14,101 @@ function renderView(module) {
 	$main.innerHTML = templates[module];
 }
 
+///////*************************** USERS
+
 function setCurrentUser(user) {
 	database.setItem('currentUser', user.username);
+
+	$header.innerHTML = `
+		<p class="small-voice currentUser">Account: ${database.getItem('currentUser')}</p>
+			`;
 }
 
-function clearCurrentUser() {
-	database.setItem('currentUser', null);
+// function clearCurrentUser() {
+// 	database.setItem('currentUser', null);
+// }
+
+function updateUsers(users) {
+
+	let stringifiedUsers = JSON.stringify(users);
+
+	database.setItem('users', stringifiedUsers);
+}
+
+// function checkUser(credentials) {
+// 	const userlistString = database.getItem('users');
+
+// 	const parsedUsers = JSON.parse(userListString);
+
+// 	parsedUsers.find( function(credentials) {
+// 		return credentials.username === user.username;
+// 	});
+// }
+
+///////*************************** ACCOUNT
+
+function handleAccountCreation(form) {
+
+	let usernameInput = form.querySelector('#username');
+	let passwordInput = form.querySelector('#password');
+
+	const username = usernameInput.value;
+	const password = passwordInput.value;
+
+	if (username && password) {
+		let user = { username, password };
+
+		var userExists = users.find( function(item) {
+			return item.username === user.username;
+		});
+
+		if (userExists) {
+			alert('choose another username');
+		} else {
+			users = [...users, user];
+
+			setCurrentUser(user);
+			updateUsers(users);
+			
+			usernameInput.value = "";
+			passwordInput.value = "";
+
+			return true; //use this to load the next page
+		}
+	}
+
+}
+
+function handleSignIn(form) {
+	let usernameInput = form.querySelector('#username');
+	let passwordInput = form.querySelector('#password');
+
+	const username = usernameInput.value;
+	const password = passwordInput.value;
+
+	if (username && password) { //if both inputs have a valid value
+		let user = { username, password }; //create an object to pass into checker
+
+		var foundUser = users.find( function(item) { //check whether user exists in database
+			return item.username === user.username;
+		});
+
+		if (foundUser) { //if user does exist in database
+			if (foundUser.password === user.password) {
+				setCurrentUser(user);
+				alert('success!');
+				return true; //use this to load the next page
+			} else {
+				alert('The password you entered does not match the username');
+			}
+
+		} else {
+			
+			alert("the username you entered does not exist");
+		}
+	} else {
+		alert('please enter a username and password');
+	}
 }
 
 //*************************** PAGE STRUCTURE
@@ -41,48 +136,12 @@ window.addEventListener('click', function(event) {
 	}
 });
 
-// function startupApp() {
-// 	users = [];
-// }
-
 let users = [
-	{username : 'derek',
-	password : '1234'}
+	{username : 'derek', password : '1234'},
+	{username: 'santa', password : 'claus'}
 ];
 
-function handleAccountCreation(form) {
 
-	let usernameInput = form.querySelector('#username');
-	let passwordInput = form.querySelector('#password');
-
-	const username = usernameInput.value;
-	const password = passwordInput.value;
-
-	if (username && password) {
-		let user = { username, password };
-
-		var userExists = users.find( function(item) {
-			return item.username === user.username;
-		});
-
-		if (userExists) {
-			alert('choose another username');
-		} else {
-			users = [...users, user];
-
-			setCurrentUser(user);
-			
-			usernameInput.value = "";
-			passwordInput.value = "";
-
-			$header.innerHTML = `<p class="small-voice">${database.getItem('currentUser')}</p>
-			`;
-
-			return true; //use this to load the next page
-		}
-	}
-
-}
 
 
 
@@ -97,12 +156,16 @@ window.addEventListener('submit', function(event) {
 			var handle = handleAccountCreation(form);
 
 			if (handle === true) {
-				renderView('signIn');
+				renderView('home');
 			}
 		}
 
 		if (formTitle === 'signIn') {
-			// handle sign in
+			handle = handleSignIn(form);
+
+			if(handle === true) {
+				renderView('home');
+			}
 		}
 	}
 
