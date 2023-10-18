630f452e81e1f4abd307bf6506190ad00133dc65
adrajagopal <adrajagopal@gmail.com>
2023-10-17 21:36:05 -0500

    Add data attributes to every template

--- projects/goals-app/templates.js
+++ projects/goals-app/templates.js
@@ -1,7 +1,7 @@
 export const templates = { };
 
 templates.signIn = `
-	<h1 class="xxl-voice" style="color: red;">SIGN IN PAGE</h1>
+	<h1 class="xxl-voice" style="color: firebrick;">SIGN IN PAGE</h1>
 
 	<form data-form="signIn">
 		<label for="username">Enter username</label>
@@ -17,7 +17,7 @@ templates.signIn = `
 `;
 
 templates.createAccount = `
-	<h1 class="xxl-voice" style="color: blue;">CREATE ACCOUNT PAGE</h1>
+	<h1 class="xxl-voice" style="color: dodgerblue;">CREATE ACCOUNT PAGE</h1>
 
 	<form data-form="createAccount">
 		<label for="username">Choose a username</label>
@@ -32,15 +32,10 @@ templates.createAccount = `
 	<p>Existing user? <button data-view="signIn">Sign in</button>.</p>
 `;
 
-templates.home = `
-	<h1>Welcome!</h1>
-
-`;
-
 templates.newUserWelcome = `
-	<h1>Why use this app?</h1>
+	<h1 class="xxl-voice">Why use this app?</h1>
 
-	<button>Let's get started</button>
+	<button class="link" data-view="intakeInstructions">Let's get started</button>
 
 `;
 
@@ -49,46 +44,68 @@ templates.newUserWelcome = `
 templates.intakeInstructions = `
 	<h1>How this works</h1>
 
-	<button>Define my sunk tasks</button>
+	<button class="link" data-view="defineSunkTasks">Define my sunk tasks</button>
 `;
 
 templates.defineSunkTasks = `
 	<h1>Define your sunk tasks</h1>
 
-	<button>Assign time to sunk tasks</button>
+	<form data-form="defineSunkTasks">
+
+		<button type="submit" data-view="estimateSunkTasks">Assign time to sunk tasks</button>
+	</form>
 `;
 
 templates.estimateSunkTasks = `
-	<h1>Define your sunk tasks</h1>
+	<h1>Estimate your sunk tasks</h1>
 
-	<button>Go to the next section</button>
+	<form data-form="estimateSunkTasks">
+
+		<button type="submit" data-view="goalsInstructions">Go to the next section</button>
+	</form>
 `;
 
 templates.goalsInstructions = `
 	<h1>How this works</h1>
 
-	<button>Define my goal areas</button>
+	<button class="link" data-view="defineGoalAreas">Define my goal areas</button>
 `;
 
 templates.defineGoalAreas = `
 	<h1>Define your goal areas</h1>
 
-	<button>Assign time to goal areas</button>
+	<form data-form="defineGoalAreas">
+
+
+		<button type="submit" data-view="estimateGoalAreas">Assign time to goal areas</button>
+	</form>
 `;
 
 templates.estimateGoalAreas = `
-	<h1>Define your goal areas</h1>
+	<h1>Estimate your goal areas</h1>
 
-	<button>Review your day</button>
+	<form data-form="estimateGoalAreas">
+
+
+		<button type="submit" data-view="intakeCompleted">Review and finalize</button>
+	</form>
 `;
 
 templates.intakeCompleted = `
 	<h1>Nice job!</h1>
 
-	<button>Review your day</button>
+	<button class="link" data-view="yourDaySummarized">See how your day shakes out</button>
 `;
 
-//*****************DAY SUMMARY FLOW
+//***************** HOME FLOW
+templates.home = `
+	<h1 class="xxl-voice">Welcome back, Aditya</h1>
+
+	<button class="link" data-view="newReflection">Add a reflection</button>
+	<button class="link" data-view="reflectionHistory">See prior reflections</button>
+	<button class="link" data-view="makeAdjustments">View/change tasks/goals and times</button>
+`;
+
 
 templates.yourDaySummarized = `
 	<h1>Here's how your day shakes out</h1>
@@ -101,8 +118,8 @@ templates.yourDaySummarized = `
 
 	<p>You're <span>YY hours short/long</span>.</p>	
 
-	<button>Make adjustments</button>
-	<button>Go home</button>
+	<button class="link" data-view="makeAdjustments">Make adjustments</button>
+	<button class="link" data-view="home">Go home</button>
 `;
 
 templates.makeAdjustments = `
@@ -115,35 +132,44 @@ templates.makeAdjustments = `
 		<li>tip 3</li>
 	</ul>
 
-	<button>Review your day</button>
+	<form data-form="makeAdjustments">
+
+
+		<button type="submit" data-view="yourDaySummarized">Review your day</button>
+	</form>
 `;
 
 //*****************NOTES FLOW
 
-templates.newNote = `
+templates.newReflection = `
 	<h1>How did you do today?</h1>
 
 	<h2>Tuesday, October 17th</h2>
 
-	<label>Rate your day</label>
-	<input type="range">
+	<form data-form="newReflection">
+		<label>Rate your day</label>
+		<input type="range">
 
-	<label>Reflect on the day</label>
-	<textarea name="" id="" cols="30" rows="10"></textarea>>
+		<label>Reflect on the day</label>
+		<textarea name="" id="" cols="30" rows="10"></textarea>
 
-	<button>Discard</button>
-	<button>Save note</button>
+		<button type="button" class="link" data-view="home">Discard</button>
+		<button type="submit" data-view="reflectionSaved">Save note</button>
+	</form>
+
+
 `;
 
-templates.noteSaved = `
+templates.reflectionSaved = `
 	<h1>Great reflection!</h1>
 
 	<p>Every reflection is an opportunity to make small changes and improve tomorrow.</p>
 
-	<button>See your prior days</button>
+	<button class="link" data-view="reflectionHistory">See your prior day reflections</button>
+	<button class="link" data-view="home">Go home</button>
 `;
 
-templates.noteHistory = `
+templates.reflectionHistory = `
 	<h1>Your saved reflections</h1>
 
 	<h2>Click to view a day.</h2>
@@ -153,9 +179,11 @@ templates.noteHistory = `
 		<li>day 2</li>
 		<li>day 3</li>
 	</ul>
+
+	<button class="link" data-view="home">Go home</button>
 `;
 
-templates.noteDetails = `
+templates.reflectionDetails = `
 	<h1>Your saved reflections</h1>
 
 	<h2>Click to view a day.</h2>
@@ -165,33 +193,6 @@ templates.noteDetails = `
 		<li>day 2</li>
 		<li>day 3</li>
 	</ul>
+
+	<button class="link" data-view="home">Go home</button>
 `;
-
-//*****************HOME MENU FLOW
-
-templates.newNote = `
-	<h1>Welcome back, Aditya</h1>
-
-	<button>Add a reflection</button>
-	<button>See prior reflections</button>
-	<button>View/change tasks/goals and times</button>
-`;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
