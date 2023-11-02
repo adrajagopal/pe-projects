e5830d28bd5fbe02a8f4a7b95b62072ef4f6bc22
adrajagopal <adrajagopal@gmail.com>
2023-11-02 12:50:06 -0500

    Create bio card module

---
+++ new-site/modules/biography-card/template.php
@@ -0,0 +1,19 @@
+<biography-card>
+	<picture>
+		<img src="images/ad-portrait-blonde.jpg">
+	</picture>
+	<div class="medium-voice bio-attributes">
+		<h3><strong class="bold">Type: </strong>Normal</h3>
+		<h3><strong class="bold">Rarity: </strong>Extremely common</h3>
+		<h3><strong class="bold">Interests: </strong>Design systems, health tech, the outdoors, music, dogs</h3>
+	</div>
+
+	<text-content>
+		<h3 class="large-voice"><strong>Biography:</strong></h3>
+
+		<p>This version of Ad appears most of the time. He’s a hyper-organized, analytics-loving, former product manager with an obsessive attention to detail. He takes a lot of pride in bringing things to life, which is why he’d rather be coding than writing product briefs.</p>
+
+		<p>He loves feeling like part of a team and making your days (and meetings) a little more joyful.</p>
+	</text-content>
+
+</biography-card>
