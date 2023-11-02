9415d2fbebe63e5022eb4790ee26e51e54e1709d
adrajagopal <adrajagopal@gmail.com>
2023-11-02 12:49:59 -0500

    Create bio controls module

---
+++ new-site/modules/biography-controls/template.php
@@ -0,0 +1,9 @@
+<biography-controls>
+	<h3 class="large-voice"><strong>Get to know me.</strong></h3>
+	<p class="large-voice">Which me? Your choice.</p>
+	<div class="button-container">
+		<button>normal bio</button>
+		<button>anxious perfectionism</button>
+		<button>aggressive confidence</button>
+	</div>
+</biography-controls>
