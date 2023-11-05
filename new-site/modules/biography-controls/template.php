8386eedeb00730da35c4970ef754efaf763125e5
adrajagopal <adrajagopal@gmail.com>
2023-11-05 09:34:36 -0600

    Formatting

--- new-site/modules/biography-controls/template.php
+++ new-site/modules/biography-controls/template.php
@@ -1,6 +1,8 @@
 <biography-controls>
-	<h3 class="large-voice"><strong>get to know me.</strong></h3>
-	<p class="large-voice">which me? your choice.</p>
+	<text-content>
+		<h3 class="large-voice"><strong>get to know me.</strong></h3>
+		<p class="large-voice">which me? your choice.</p>
+	</text-content>
 	<div class="button-container">
 		<button>normal bio</button>
 		<button>anxious perfectionism</button>
