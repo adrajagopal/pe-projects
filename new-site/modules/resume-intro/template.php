dee2554af1a6634e2dc82a57c660a9de9657d2fb
adrajagopal <adrajagopal@gmail.com>
2023-11-05 18:43:00 -0600

    Add resume-intro module and update styling

---
+++ new-site/modules/resume-intro/template.php
@@ -0,0 +1,9 @@
+<div class="resume-intro">
+	<text-content>
+		<p class="medium-voice">I'm coming to webdev with 7 years of experience in analytics, consulting and product management in the healthcare space.</p>
+	
+		<p>I've always worked on small-but-growing teams, whether that's deeply embedded in a corporation of 80,000 employees, or as the first IC product manager of a pre-seed startup.</p>
+	
+		<p>My former clients, colleagues, and managers would (and have!) described me as extremely organized, attentive to detail, proactive, brilliant, and a joy to be around. What will you say?</p>
+	</text-content>
+</div>
