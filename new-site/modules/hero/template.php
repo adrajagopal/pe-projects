1caedd9ed01cc154d7ba786559cf3b99aa0cfc10
adrajagopal <adrajagopal@gmail.com>
2023-11-06 11:58:36 -0600

    Connect hero module to JSON

--- new-site/modules/hero/template.php
+++ new-site/modules/hero/template.php
@@ -1,6 +1,11 @@
+<?php 
+	$heroTitle = $pageData['leftSections'][0]['content']['heading'];
+	$heroDescription = $pageData['leftSections'][0]['content']['description'];
+?>
+
 <div class="hero-card">
-<text-content>
-	<h1 class="xxl-voice"><?= $heroTitle ?? "sample header"?></h1>
-	<p class="large-voice"><?= $heroDescription ?? "sample description"?></p>
-</text-content>
+	<text-content>
+		<h1 class="xxl-voice"><?= $heroTitle ?? "sample header"?></h1>
+		<p class="large-voice">//<?= $heroDescription ?? "sample description"?></p>
+	</text-content>
 </div>
