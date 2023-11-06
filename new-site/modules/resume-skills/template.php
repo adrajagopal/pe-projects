43cb508ca7e51648989a3d85c9269c24f81e5cec
adrajagopal <adrajagopal@gmail.com>
2023-11-05 18:42:53 -0600

    Add resume-skills module and update styling

---
+++ new-site/modules/resume-skills/template.php
@@ -0,0 +1,11 @@
+<div class="resume-skills">
+	<h2 class="xl-voice section-title">Skills</h2>
+	
+	<ul class="medium-voice skill-list">
+		<?php
+			foreach ($skills as $skill) { 
+				echo "<li class='skill'>$skill</li>";
+			} 
+		?>
+	</ul>
+</div>
