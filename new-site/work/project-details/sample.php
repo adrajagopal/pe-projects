1e87264e3d9eba4911a9369eb7c5da2059e948fc
adrajagopal <adrajagopal@gmail.com>
2023-11-06 11:15:18 -0600

    Create project details page structure

---
+++ new-site/work/project-details/sample.php
@@ -0,0 +1,66 @@
+<?php include('../../header.php'); ?>
+
+<article>
+	<inner-column>
+		<section>
+			<h2 class="large-voice"><strong>brief</strong></h2>
+			<p>I was handed a folder of HTML and PHP files and was tasked with styling it like a professional website – borrowing heavily from an industry of my choosing – over the course of a one-week sprint.</p>
+
+			<figure>
+				<picture style="max-width: 300px;"><img src="images/name-logo.svg" alt=""></picture>
+				<figcaption>Here's the logo</figcaption>
+			</figure>
+
+		</section>
+		
+		<section>
+			<h2 class="large-voice"><strong>challenges</strong></h2>
+			<p>
+				Design was definitely the most challenging piece of this project. The goal of the project was to “steal” from industry peers, but then take that and create something unique, yet familiar. It took lots of feedback and iteration to get it to a place where my design language communicated what I needed it to, while also feeling fresh and interesting.
+			</p>
+		
+			<p>
+				This project also pushed my knowledge on CSS grid and working with modules – in other words, making sure my scoping was airtight so my styles didn’t leak all over the place.
+			</p>
+		</section>
+		
+		<section>
+			<h2 class="large-voice"><strong>process</strong></h2>
+			<p>
+				The first step was to write the CSS for the layout, so that I would have a responsive wireframe to work with.
+			</p>
+		
+			<p>
+				Next, I analyzed 10-15 digital health companies’ marketing websites, and narrowed that list down to a group of 6 favorites to compare their color palettes, brand copy, structure, and overall tone.
+			</p>
+
+			<figure>
+				<picture style="max-width: 300px;"><img src="images/name-logo.svg" alt=""></picture>
+				<figcaption>Here's the logo</figcaption>
+			</figure>
+		
+			<p>From there, I created style tiles – a loose deliverable that’s used to communicate the overall vision and feeling of a design, with clear definition of typography, color, and some sample modules.</p>
+
+			<figure>
+				<picture style="max-width: 300px;"><img src="images/name-logo.svg" alt=""></picture>
+				<figcaption>Here's the logo</figcaption>
+			</figure>
+
+			<p>I got feedback from around 7 people over the course of 2 days, and iterated quite a bit on my style tile. At that point, I cranked out the CSS.</p>
+			<p>I actually finished early – and spent that time creating a brand new module. I wrote the HTML, CSS, and also created a set of icons to use.</p>
+		</section>
+		
+		<section>
+			<h2 class="large-voice"><strong>learnings</strong></h2>
+			<p>This project was a test of the prior 2 months of learning the ins and outs of HTML, CSS, typography, graphics & image preparation, and PHP. It pushed my abilities in all of those areas and I learned a lot.</p>
+			<p>I got very comfortable working with CSS grids, learned the value of modular code and reusable components, creating icons that are optically aligned, and learned how to iterate through design concepts and take an idea from a loose collection of bullet-points to a polished website.</p>
+
+			<figure>
+				<picture style="max-width: 300px;"><img src="images/name-logo.svg" alt=""></picture>
+				<figcaption>Here's the logo</figcaption>
+			</figure>
+		</section>
+	</inner-column>
+
+
+</article>
