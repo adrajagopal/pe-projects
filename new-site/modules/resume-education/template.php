8924ff6fedb74cf5c4efc494bbfba63d529d859f
adrajagopal <adrajagopal@gmail.com>
2023-11-05 18:43:06 -0600

    Add resume-education module and update styling

---
+++ new-site/modules/resume-education/template.php
@@ -0,0 +1,18 @@
+<div class="resume-education">	
+
+	<h2 class="xl-voice section-title">Education</h2>
+
+	<div class="item">
+
+		<h3 class="large-voice">Perpetual Education</h3>
+
+		<p class="medium-voice degree-title">Program: Design for the Web</p>
+	</div>
+
+	<div class="item">
+		<h3 class="large-voice">University of Florida, Gainesville</h3>
+
+		<p class="medium-voice degree-title">BA Economics, Cum Laude</p>
+	</div>
+
+</div>
