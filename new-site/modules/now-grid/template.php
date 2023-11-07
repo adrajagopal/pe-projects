04c8f752885f0e1d237eb1b6546073902543900a
adrajagopal <adrajagopal@gmail.com>
2023-11-06 19:23:23 -0600

    Create now grid module and style

---
+++ new-site/modules/now-grid/template.php
@@ -0,0 +1,18 @@
+<?php
+	$json = file_get_contents('data/nowList.json');
+
+	$nowGroups = json_decode($json, true);
+?>
+
+<ul class="now-grid">
+	
+<?php
+	foreach ($nowGroups as $nowGroup) {
+		$category = $nowGroup['category'];
+		$items = $nowGroup['items'];
+
+		include(getFile('modules/now-card/template.php'));
+	}
+?>
+
+</ul>
