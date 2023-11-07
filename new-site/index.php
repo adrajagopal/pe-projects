ce515c3ccbb7c6d95537d1750241a50a34090bca
adrajagopal <adrajagopal@gmail.com>
2023-11-07 13:56:08 -0600

    Built new router for projectDetails pages

--- new-site/index.php
+++ new-site/index.php
@@ -7,45 +7,92 @@
 
 	$json = file_get_contents("data/pages/$page.json");
 
-	$pageData = json_decode($json, true);
+	if ($page === "projectDetails") {
+		if (isset($_GET["projectId"])) {
+			$projectId = $_GET["projectId"];
 
+			$json = file_get_contents("data/projects/$projectId.json");
+		}
+	}
+
+	$pageData = json_decode($json, true);
 ?>
 
 <?php include("header.php");?>
 
-<div class="page-wrapper <?=$page?>">
-	<left-side>
-		<div class="left-content">
+<?php if ($page != "projectDetails") { ?> 
 
-			<?php
-				foreach ($pageData['leftSections'] as $section) { ?>
-					<section class="<?=$section['name']?>">
-						<inner-column>
-							<?php include(getFile("modules/$section[name]/template.php")); ?>
-						</inner-column>
-					</section>
+	<div class="page-wrapper <?=$page?>">
+		<left-side>
+			<div class="left-content">
 
-			<?php } ?>
+				<?php
+					foreach ($pageData['leftSections'] as $section) { ?>
+						<section class="<?=$section['name']?>">
+							<inner-column>
+								<?php include(getFile("modules/$section[name]/template.php")); ?>
+							</inner-column>
+						</section>
 
-		</div>
-	</left-side>
+				<?php } ?>
 
-	<right-side>
-		<div class="right-content">
-		<main>
-			<?php
-				foreach ($pageData['rightSections'] as $section) { ?>
-					<section class="<?=$section['name']?>">
-						<inner-column>
-							<?php include(getFile("modules/$section[name]/template.php")); ?>
-						</inner-column>
-					</section>
+			</div>
+		</left-side>
 
-			<?php } ?>
-		</main>
-		</div>
-	</right-side>
-</div> <!-- end .page-wrapper -->
+		<right-side>
+			<div class="right-content">
+			<main>
+				<?php
+					foreach ($pageData['rightSections'] as $section) { ?>
+						<section class="<?=$section['name']?>">
+							<inner-column>
+								<?php include(getFile("modules/$section[name]/template.php")); ?>
+							</inner-column>
+						</section>
+
+				<?php } ?>
+			</main>
+			</div>
+		</right-side>
+	</div> <!-- end .page-wrapper -->
+<?php } ?>
+
+<?php if ($page === "projectDetails") { ?> 
+
+	<div class="page-wrapper <?=$page?>">
+		<left-side>
+			<div class="left-content">
+
+				<section class="hero">
+					<inner-column>
+						<?php include(getFile("modules/hero/template.php")); ?>
+					</inner-column>
+				</section>
+
+			</div>
+		</left-side>
+
+		<right-side>
+			<div class="right-content">
+			<main>
+				<?php
+					foreach ($pageData['sections'] as $section) { ?>
+						<section class="<?=$section['title']?>">
+							<inner-column>
+								<?php 
+									foreach ($section['modules'] as $module) {
+										include(getFile("modules/project-details-$module[type]/template.php"));
+									}
+								?>
+							</inner-column>
+						</section>
+
+				<?php } ?>
+			</main>
+			</div>
+		</right-side>
+	</div> <!-- end .page-wrapper -->
+<?php } ?>
 
 <?php include("site-footer.php");?>
 
