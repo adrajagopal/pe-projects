a0f72f9d61bc1f1cdd72f0f0b8fb44e84db15a8f
adrajagopal <adrajagopal@gmail.com>
2023-11-07 13:56:43 -0600

    Cleanup and codekit compile

--- new-site/modules/projects-grid/template.php
+++ new-site/modules/projects-grid/template.php
@@ -1,1 +1,5 @@
 template.php
+
+<!-- Design: Using HTML & CSS to build responsive layouts, accessible content, and design systems
+
+Programming: Using JavaScript and PHP to build interactive UIs -->
