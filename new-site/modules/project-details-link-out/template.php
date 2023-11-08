bc406ae470674b0225a3ba6332ea09f2ab80dcc0
adrajagopal <adrajagopal@gmail.com>
2023-11-07 18:06:48 -0600

    Create link out button component

---
+++ new-site/modules/project-details-link-out/template.php
@@ -0,0 +1,6 @@
+<div class="link-out">
+	<a href="projects/<?=$pageData['liveDemo']?>" target="_blank" class="medium-voice">
+		<?=$module['linkCopy']?>
+		<svg class="icon-"><use xlink:href="#icon-external-link" y="0.2rem"></use></svg>
+	</a>
+</div>
