24e889ac4d17e77b633291fb5f07069b914a66bf
adrajagopal <adrajagopal@gmail.com>
2023-11-08 18:49:46 -0600

    Refactor HTML and style project card - sux still

--- new-site/modules/projects-project-card/template.php
+++ new-site/modules/projects-project-card/template.php
@@ -1,6 +1,11 @@
 <div class="project-card">
-	<text-content>
-		<h3 class="med-plus-voice"><?= $detail['title'] ?? "sample header"?></h3>
-		<p>//<?= $detail['oneLiner'] ?? "This is the first sentence/paragraph of the project case study. This is what it looks like."?></p>
-	</text-content>
+
+		<div class="top-banner">
+			<h3 class="large-voice"><?= $detail['title'] ?? "sample header"?></h3>
+		</div>
+		<div class="bottom-section">
+			<p><?= $detail['oneLiner'] ?? "This is the first sentence/paragraph of the project case study. This is what it looks like."?></p>
+			<a href="?page=projectDetails&projectId=<?=$detail['projectId']?>">read more</a>
+		</div>
+		
 </div>
