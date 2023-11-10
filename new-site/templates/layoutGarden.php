4f2e86a976874cb19f76595e1c1b87fc5566e7d0
adrajagopal <adrajagopal@gmail.com>
2023-11-10 09:25:02 -0600

    Refactor layout garden to fit new structure

---
+++ new-site/templates/layoutGarden.php
@@ -0,0 +1,13 @@
+<div class="page-wrapper <?=$page?>">
+	<main>
+		<?php
+			foreach ($pageData['sections'] as $section) { ?>
+				<section class="<?=$section['name']?>">
+					<inner-column>
+						<?php include(getFile("layout-garden-modules/$section[name]/template.php")); ?>
+					</inner-column>
+				</section>
+
+		<?php } ?>
+	</main>
+</div> <!-- end .page-wrapper -->
