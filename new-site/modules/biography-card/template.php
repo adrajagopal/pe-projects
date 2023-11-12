f3be757bcaf3e9cd33559d426a5f825a7cd59180
adrajagopal <adrajagopal@gmail.com>
2023-11-12 10:37:05 -0600

    Update styling and move html to JS file

--- new-site/modules/biography-card/template.php
+++ new-site/modules/biography-card/template.php
@@ -1,27 +1,4 @@
 <biography-card>
-	<picture>
-		<img src="images/ad-portrait-blonde.jpg">
-	</picture>
-	<ul class="medium-voice bio-attributes">
-		<li>
-			<h3><strong class="bold">type: </strong>normal</h3>
-		</li>
-		<li>
-			<h3><strong class="bold">rarity: </strong>extremely common</h3>
-		</li>
-		<li>
-			<h3><strong class="bold">interests: </strong>design systems, health tech, the outdoors, music, dogs</h3>
-		</li>
-	</ul>
-
-	<text-content>
-		<h3 class="large-voice"><strong>biography:</strong></h3>
-
-		<p>this version of Ad appears most of the time. he’s a hyper-organized, analytics-loving, former product manager with an obsessive attention to detail. he takes a lot of pride in bringing things to life himself, which is why he’d rather be coding than writing product briefs.</p>
-
-		<p>he loves feeling like part of a team and making your days (and meetings) a little more joyful.</p>
-	</text-content>
-
-	<button>check out my work</button>
-
+	<!-- outlet -->
 </biography-card>
+<script src="modules/biography-card/controller.js"></script>
