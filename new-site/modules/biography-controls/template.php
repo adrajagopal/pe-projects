50f019931b4a149d3a07bf10c55a304ea91a6a23
adrajagopal <adrajagopal@gmail.com>
2023-11-12 10:36:37 -0600

    Update bio-controls style and copy

--- new-site/modules/biography-controls/template.php
+++ new-site/modules/biography-controls/template.php
@@ -1,22 +1,22 @@
 <biography-controls>
 	<text-content>
-		<h3 class="large-voice"><strong>get to know me.</strong></h3>
+		<h3 class="large-voice"><strong>Choose your vibe:</strong></h3>
 		<!-- <p class="medium-voice">which me? your choice.</p> -->
 	</text-content>
 	<form class="button-container">
 		<div class="option">
-			<input id="normal" type="radio" name="bio-control">
-			<label for="normal">normal bio</label>
+			<input id="normal" type="radio" name="bio-control" checked="checked">
+			<label for="normal">Classic</label>
 		</div>
 
 		<div class="option">
 			<input id="anxious" type="radio" name="bio-control">
-			<label for="anxious">anxious perfectionist</label>
+			<label for="anxious">Anxious perfectionist</label>
 		</div>
 
 		<div class="option">
 			<input id="aggressive" type="radio" name="bio-control">
-			<label for="aggressive">aggressively confident</label>
+			<label for="aggressive">Aggressively confident</label>
 		</div>
 	</form>
 </biography-controls>
