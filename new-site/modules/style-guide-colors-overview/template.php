24ec26eb98a4bebb40aaacbbdb6f9579b1254fa8
adrajagopal <adrajagopal@gmail.com>
2023-11-12 09:03:42 -0600

    Add paper-grey to custom properties

--- new-site/modules/style-guide-colors-overview/template.php
+++ new-site/modules/style-guide-colors-overview/template.php
@@ -1,9 +1,10 @@
 <div class="colors-overview">
 	<div class="color-grid">
-		<div class="color medium-voice">base hue</div>
-		<div class="color medium-voice">paper</div>
-		<div class="color medium-voice">paper accent</div>
-		<div class="color medium-voice">ink</div>
-		<div class="color medium-voice">ink grey</div>
+		<div class="color medium-voice baseHue">base hue</div>
+		<div class="color medium-voice paper">paper</div>
+		<div class="color medium-voice paperAccent">paper accent</div>
+			<div class="color medium-voice paperGrey">paper grey</div>
+		<div class="color medium-voice ink">ink</div>
+		<div class="color medium-voice inkGrey">ink grey</div>
 	</div>
 </div>
