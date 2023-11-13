87b9e41af1d34d7c642c35af34ba5231addd686e
adrajagopal <adrajagopal@gmail.com>
2023-11-12 20:00:04 -0600

    Update google fonts

--- new-site/header.php
+++ new-site/header.php
@@ -35,8 +35,7 @@
 <!-- 	fonts used:
 Inter -->
 
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;700&display=swap" rel="stylesheet">
-
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
 </head>
 
