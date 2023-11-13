6fa564e012efb14f48fb076e6de34ffcf61532cc
adrajagopal <adrajagopal@gmail.com>
2023-11-12 20:00:27 -0600

    Drop in placeholder html for bio card

--- new-site/modules/biography-card/template.php
+++ new-site/modules/biography-card/template.php
@@ -1,4 +1,28 @@
 <biography-card>
 	<!-- outlet -->
+	<picture>
+			<img src="images/ad-portrait-blonde.jpg">
+	</picture>
+	<ul class="medium-voice bio-attributes">
+		<li>
+			<h3><strong>Type:</strong> ${version.type}</h3>
+		</li>
+		<li>
+			<h3><strong>Rarity:</strong> ${version.rarity}</h3>
+		</li>
+		<li>
+			<h3><strong>Interests:</strong> ${version.interests}</h3>
+		</li>
+	</ul>
+
+	<text-content>
+		<h3 class="large-voice">Biography:</h3>
+
+		<p>${version.bioParagraph}</p>
+
+		<p>${version.closingSentence}</p>
+	</text-content>
+
+	<button>Check out my work</button>
 </biography-card>
 <script src="modules/biography-card/controller.js"></script>
