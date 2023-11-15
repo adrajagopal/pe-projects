7b5adfb97633b65d3fbec602b51c0977897c1897
adrajagopal <adrajagopal@gmail.com>
2023-11-15 13:00:38 -0600

    Add cookies disclaimer to footer

--- new-site/site-footer.php
+++ new-site/site-footer.php
@@ -7,6 +7,10 @@
 				<?php include('socials.php');?>
 			</div>
 		</inner-column>
+
+		<inner-column>
+			<p style="font-size: 6px; padding: 8px 0; transform: translateX(-6px);">I reserve the right to sell your color preferences to the highest bidder. I'm not tracking your identity, so it's useless, but maybe I can get Skittles to release a new flavor.</p>
+		</inner-column>
 			
 	</footer>
 
