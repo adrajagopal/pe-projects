6a517d29217504881450340c3b9b3ff105bdec8c
adrajagopal <adrajagopal@gmail.com>
2023-12-12 17:58:47 -0600

    test BASE_URL on prod

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<base href="<?=BASE_URL?>">
+	<!-- <base href="<?=BASE_URL?>"> -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 
