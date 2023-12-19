11f49e6ab1c45145f834daac770383d2e1ed3adf
adrajagopal <adrajagopal@gmail.com>
2023-12-18 21:38:22 -0600

    Use component for link-out

--- modules/project-details-link-out/template.php
+++ modules/project-details-link-out/template.php
@@ -1,6 +1,9 @@
+<?php
+	$url = "projects/$pageData[liveDemo]";
+	$copy = $module['linkCopy'];
+?>
+
+
 <div class="link-out">
-	<a href="projects/<?=$pageData['liveDemo']?>" target="_blank" class="medium-voice">
-		<?=$module['linkCopy']?>
-		<svg class="icon-"><use xlink:href="#icon-external-link" y="0.1rem"></use></svg>
-	</a>
-</div>
+	<?php include(getFile('components/external-link/template.php'));?>
+</a>
