9c62e81c615cf10492c55879f0779d0cb451c700
adrajagopal <adrajagopal@gmail.com>
2024-01-10 00:21:07 -0600

    Update resume copy

--- modules/resume-intro/template.php
+++ modules/resume-intro/template.php
@@ -1,8 +1,8 @@
 <div class="resume-intro">
 	<text-content>
-		<p class="medium-voice">I'm coming to webdev with 7 years of experience in analytics, consulting and product management in the healthcare space.</p>
+		<p class="medium-voice">I'm a frontend developer with 7 years of experience in consulting and product management in the healthcare space.</p>
 	
-		<p>I've always worked on small-but-growing teams, whether that's deeply embedded in a corporation of 80,000 employees, or as the first IC product manager of a seed-stage startup.</p>
+		<p>I've taken products and features from brainstorm to launch, helping companies find patients with rare diseases, helping gastrointestinal patients connect with their providers virtually, and working with designers, engineers and data scientists to get it all done.</p>
 	
 		<p>My former clients, colleagues, and managers have described me as "the most organized person I've ever met", attentive to detail, proactive, brilliant, and a joy to be around.</p>
 
