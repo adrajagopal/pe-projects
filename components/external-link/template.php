6576b2da20a429c9a0570cf0306050f075ccb286
adrajagopal <adrajagopal@gmail.com>
2024-01-11 19:55:28 -0600

    Formatting

--- components/external-link/template.php
+++ components/external-link/template.php
@@ -1,5 +1,5 @@
 <a href="<?=$url?>" target="_blank" class="external-link-component">
 	<?=$copy?> 
-	<svg class="icon-"><use xlink:href="#icon-external-link" y="0.1rem"></use></svg>
+	<svg class="icon-"><use xlink:href="#icon-external-link" y="0rem"></use></svg>
 </a>
 
