c6bd8cd3b70ddd9f636f499350eee01eb0538889
adrajagopal <adrajagopal@gmail.com>
2024-01-17 13:46:14 -0600

    Create util to log focused element

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 export const palettes = document.querySelector('.palettes');
 export const body = document.querySelector('body');
+
+// document.addEventListener('focus', (event) => {
+// 	console.log('Focused element:', event.target);
+// }, true); // Using true for capturing phase
