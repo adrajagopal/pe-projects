58850c26959b73754849590bf907ef72d2e7ab7e
adrajagopal <adrajagopal@gmail.com>
2024-01-20 13:03:24 -0600

    Update figure alt text ?? placeholder text

--- modules/project-details-figure/template.php
+++ modules/project-details-figure/template.php
@@ -1,6 +1,6 @@
 <div class="project-figure">
 	<figure>
-		<picture><img src="images/<?=$module['src']?>" alt="<?=$module['src'] ?? "Photo alt text"?>"></picture>
+		<picture><img src="images/<?=$module['src']?>" alt="<?=$module['alt'] ?? "A screenshot illustrating the prior paragraph."?>"></picture>
 		<figcaption class="small-voice"><?=$module['figcaption'] ?? "Sample figcaption"?></figcaption>
 	</figure>
 </div>
