b2035acfa116237af657e43e2df7d1748db60135
adrajagopal <adrajagopal@gmail.com>
2024-01-22 19:31:36 -0600

    copy update

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<base href="<?=BASE_URL?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Ad Does Dev & Design</title>
+	<title>Ad - Frontend + Design</title>
 	<link rel="icon" type="image/x-icon" href="images/favicon.svg">
 
 	<meta name="description" content="Product- & design-focused frontend dev">
