5614711d8a8c3fcf75874d637178adc6f1fb45e7
adrajagopal <adrajagopal@gmail.com>
2024-02-01 12:34:08 -0600

    Add transition to FAQ icon; clean up SCSS

--- layout-garden-modules/faq-card/template.php
+++ layout-garden-modules/faq-card/template.php
@@ -1,13 +1,13 @@
 <faq-card>
 	<details class="faq-card">
 		<summary class="medium-voice">
-			<?=$faq['question']?>
+			<p><?=$faq['question']?></p>
 			<div class="icon-container">
 				<svg class="svg-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M9 18L15 12L9 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg>
 			</div>
 		</summary>
-		<p class="small-voice"><?=$faq['answer']?></p>
+		<p class="answer small-voice"><?=$faq['answer']?></p>
 	</details>
 </faq-card>
