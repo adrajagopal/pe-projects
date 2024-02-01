21770c349c0108e7d5d272fbacabbdd4d9ee00e5
adrajagopal <adrajagopal@gmail.com>
2024-02-01 17:26:07 -0600

    Delete old files

--- component-library-modules/layout-garden-heading/template.php
+++
@@ -1,8 +0,0 @@
-
-<garden-heading style="display:block;">
-	<text-content>
-		<h1 class="xxl-voice">Component library</h1>
-		
-		<p>This page is an exploration of components and modules used commonly across the web. They're all responsive, accessible, and adhere to the design system of this site.</p>
-	</text-content>
-</garden-heading>
