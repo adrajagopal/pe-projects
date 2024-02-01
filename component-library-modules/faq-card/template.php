07b2d0d0d4a5daa9d2369cefa7fc499d39c23538
adrajagopal <adrajagopal@gmail.com>
2024-02-01 17:25:23 -0600

    Update faq-card for style consistency

--- component-library-modules/faq-card/template.php
+++ component-library-modules/faq-card/template.php
@@ -1,13 +1,13 @@
 <faq-card>
 	<details class="faq-card">
 		<summary class="medium-voice">
-			<p><?=$faq['question']?></p>
+			<p class="question"><?=$faq['question']?></p>
 			<div class="icon-container">
 				<svg class="svg-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M9 18L15 12L9 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg>
 			</div>
 		</summary>
-		<p class="answer small-voice"><?=$faq['answer']?></p>
+		<p class="answer"><?=$faq['answer']?></p>
 	</details>
 </faq-card>
