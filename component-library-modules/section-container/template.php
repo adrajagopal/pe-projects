cf24655bdb304ed4a40072dc5ce5de431c728feb
adrajagopal <adrajagopal@gmail.com>
2024-02-01 19:25:27 -0600

    Style section accordion container

--- component-library-modules/section-container/template.php
+++ component-library-modules/section-container/template.php
@@ -1,12 +1,12 @@
 <div class="category-dropdown">
 	<details>
 		<summary class="medium-voice">
-			<h2 class="xl-voice question"><?=$section['name'] ?? "Section Header"?></h2>
 			<div class="icon-container">
-				<svg class="svg-icon" width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<svg class="svg-icon" width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M9 18L15 12L9 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg>
 			</div>
+			<h2 class="xl-voice question"><?=$section['name'] ?? "Section Header"?></h2>
 		</summary>
 		<ul class="module-list">
 			<?php
